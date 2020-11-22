6ef3374fca0298a3b7576cb8731ae82cf414af83
Manan Tank <manantankm@gmail.com>
2020-11-22 12:42:49 +0530

    add lifecycles

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import { bindText, bindAttributeValue } from './utils/bind.js';
 import addState from './utils/state.js';
 import buildShadowDOM from './utils/buildShadowDOM.js';
 import { processNode, processAttributes, processText } from './utils/process.js';
+import getAttributes from './utils/getAttributes.js';
 
 function $(elementName, options) {
+	const template = document.createElement('template');
+	const style = options.css ? `<style> ${options.css}</style>` : '';
+	template.innerHTML = options.html + style;
+
 	class El extends HTMLElement {
 		constructor() {
 			super();
@@ -12,7 +17,25 @@ function $(elementName, options) {
 			this.props = options.props;
 			this.options = options;
 			this.addState();
-			this.buildShadowDOM();
+			this.buildShadowDOM(template);
+		}
+
+		connectedCallback() {
+			if (options.onConnect) options.onConnect.call(this);
+		}
+
+		disconnectedCallback() {
+			if (options.onDisconnect) options.onDisconnect.call(this);
+		}
+
+		static get observedAttributes() {
+			return options.props || [];
+		}
+
+		attributeChangedCallback(attr, oldVal, newVal) {
+			if (options.onPropChange) {
+				onPropChange(attr, newVal);
+			}
 		}
 	}
 
@@ -22,7 +45,6 @@ function $(elementName, options) {
 	El.prototype.bindAttributeValue = bindAttributeValue;
 	El.prototype.buildShadowDOM = buildShadowDOM;
 	El.prototype.addState = addState;
-
 	El.prototype.processNode = processNode;
 	El.prototype.processAttributes = processAttributes;
 	El.prototype.processText = processText;
