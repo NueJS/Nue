689388a73c5f3740a895f46bf43f6733520f7384
Manan Tank <manantankm@gmail.com>
2020-11-22 13:06:17 +0530

    remove useless console.warn

--- src/utils/onChange.js
+++ src/utils/onChange.js
@@ -1,3 +1,5 @@
+import { silentMutate } from './mutate.js';
+
 function onChange(chain, value, trap) {
 	const success = silentMutate(this.state, chain, value, trap);
 	const chainString = chain.join('.');
@@ -5,15 +7,11 @@ function onChange(chain, value, trap) {
 	if (cbs) {
 		cbs.forEach(d => d());
 		if (this.options.onStateChange) this.options.onStateChange.call(thisVal, chainString);
-	} else {
-		console.warn(
-			`state.${chain} is being updated in component, but is not being used. Either use it in component or remove it from state`
-		);
 	}
 	return success;
 }
 
-export function notifyDeps(key, cb) {
+export function onStateChange(key, cb) {
 	if (!this.deps[key]) this.deps[key] = [];
 	this.deps[key].push(cb);
 }
