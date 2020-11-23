99aaa929a91dc067c8e16acc7b4bcecc4248794f
Manan Tank <manantankm@gmail.com>
2020-11-23 11:41:43 +0530

    populate state from props

--- src/utils/state.js
+++ src/utils/state.js
@@ -2,8 +2,13 @@ import getRS from './getRS.js';
 
 function addState() {
 	// if the state is a function, call with props to get the state object
-	const initState =
-		typeof this.options.state === 'function' ? this.options.state(this.props) : this.options.state;
+	const initState = this.options.state;
+	for (const prop of this.observedProps) {
+		const propValue = this.getAttribute(prop);
+		if (propValue) {
+			initState[prop] = propValue;
+		}
+	}
 	// reactify state object
 	this.state = getRS(initState, this.onChange.bind(this));
 }
