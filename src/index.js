6b608c55fca73d0516196fa42dc83b0724211332
Manan Tank <manantankm@gmail.com>
2020-11-23 11:46:45 +0530

    props create initial state, dispatch custom events

--- src/index.js
+++ src/index.js
@@ -7,19 +7,20 @@ import processNode from './utils/processNode.js';
 import processAttributes from './utils/processAttributes.js';
 import processTextContent from './utils/processTextContent.js';
 import processMapping from './utils/processMapping.js';
-import computeProps from './utils/computeProps.js';
 
 function $(elementName, options) {
 	const template = document.createElement('template');
 	const style = options.css ? `<style> ${options.css}</style>` : '';
 	template.innerHTML = options.html + style;
 
+	const observedProps = Object.keys(options.state);
+
 	class El extends HTMLElement {
 		constructor() {
 			super();
 			this.deps = {};
 			this.options = options;
-			this.computeProps();
+			this.observedProps = observedProps;
 			this.addState();
 			this.buildShadowDOM(template);
 		}
@@ -33,20 +34,17 @@ function $(elementName, options) {
 		}
 
 		static get observedAttributes() {
-			return options.props || [];
+			return observedProps || [];
 		}
 
-		attributeChangedCallback(attr, oldVal, newVal) {
-			if (options.onPropChange) {
-				onPropChange(attr, newVal);
-			}
+		attributeChangedCallback(prop, oldVal, newVal) {
+			this.state[prop] = newVal;
 		}
 	}
 
 	El.prototype.onChange = onChange;
 	El.prototype.onStateChange = onStateChange;
 	El.prototype.addState = addState;
-	El.prototype.computeProps = computeProps;
 	// binding
 	El.prototype.bindText = bindText;
 	El.prototype.bindAttributeValue = bindAttributeValue;
@@ -57,6 +55,14 @@ function $(elementName, options) {
 	El.prototype.processAttributes = processAttributes;
 	El.prototype.processMapping = processMapping;
 	El.prototype.processTextContent = processTextContent;
+
+	El.prototype.dispatchCustomEvent = function (eventName, detail) {
+		this.dispatchEvent(
+			new CustomEvent(eventName, {
+				detail,
+			})
+		);
+	};
 
 	customElements.define(elementName, El);
 }
