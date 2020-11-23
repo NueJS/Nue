91803d5c601730e2b81c44da6f80377ae9641a0a
Manan Tank <manantankm@gmail.com>
2020-11-23 14:34:40 +0530

    call appropriate callbacks from deps

--- src/utils/onChange.js
+++ src/utils/onChange.js
@@ -1,32 +1,31 @@
 import { silentMutate } from './mutate.js';
+import getSlice from './getSlice.js';
 
 function onChange(chain, value, trap) {
 	const success = silentMutate(this.state, chain, value, trap);
-	const chainString = chain.join('.');
+	const rootKey = chain[0];
+	const isLengthChanged = chain[chain.length - 1] === 'length';
 
-	// also update props to reflect the state change
-	this.state.__disableOnChange__(true);
-	this.setAttribute(chain[0], this.state[chain[0]]);
-	this.state.__disableOnChange__(false);
-
-	// TODO improve this
-	// instead of only checking the parent
-	const cbs = this.deps[chain[0]];
-
-	// add batching too!
+	// call onStateChange with chain for valid changes
 	if (this.options.onStateChange) {
-		// do not call cbs when arr.length is updated
-		if (chain[chain.length - 1] !== 'length') this.options.onStateChange.call(this, chain[0]);
+		// ignore .length changes for array
+		if (!isLengthChanged) this.options.onStateChange.call(this, rootKey);
 	}
-	if (cbs && chain[chain.length - 1] !== 'length') {
+
+	const cbs = this.deps[rootKey];
+	if (cbs && !isLengthChanged) {
 		cbs.forEach(f => f());
 	}
+
 	return success;
 }
 
+// if deps on a.b.c
+// register it for a
 export function onStateChange(key, cb) {
-	if (!this.deps[key]) this.deps[key] = [];
-	this.deps[key].push(cb);
+	const depKey = key.split('.')[0];
+	if (!this.deps[depKey]) this.deps[depKey] = [];
+	this.deps[depKey].push(cb);
 }
 
 export default onChange;
