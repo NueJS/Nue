567a626ce04516eca7700257ffcd3549488abe22
Manan Tank <manantankm@gmail.com>
2020-11-23 14:34:05 +0530

    attribute names that start with colons are treated as props

--- src/utils/bind.js
+++ src/utils/bind.js
@@ -1,8 +1,7 @@
 import getSlice from './getSlice.js';
 import { mutate } from './mutate.js';
 
-// when a key in state changes, update the textContent of <node>
-export function bindText(node, key) {
+export function bindTextContent(node, key) {
 	this.onStateChange(key, () => {
 		node.textContent = getSlice(this.state, key);
 	});
@@ -10,19 +9,33 @@ export function bindText(node, key) {
 
 // when key in state changes, update attribute <name> of <node>
 export function bindAttributeValue(node, name, key) {
-	node.setAttribute(name, getSlice(this.state, key));
-	this.onStateChange(key, () => {
-		node.setAttribute(name, getSlice(this.state, key));
-	});
+	const value = getSlice(this.state, key);
+	const isProp = name[0] === ':';
+	const propName = name.substr(1);
+	if (isProp) {
+		if (!node.props) node.props = {};
+		node.props[propName] = value;
+		node.removeAttribute(name);
+		this.onStateChange(key, () => {
+			node.state[propName] = getSlice(this.state, key);
+		});
+	} else {
+		node.setAttribute(name, value);
+		this.onStateChange(key, () => {
+			node.setAttribute(name, getSlice(this.state, key));
+		});
+	}
 }
 
 export function bindInput(node, attributeName, attributeValue) {
 	if (attributeName === 'value') {
 		const eventName = 'input';
+		const attributeValueSplit = attributeValue.split('.');
+		const isNumber = node.type === 'number' || node.type === 'range';
 		const handler = e => {
-			const value = e.target[attributeName];
-			const isNumber = node.type === 'number' || node.type === 'range';
-			mutate(this.state, attributeValue.split('.'), isNumber ? Number(value) : value, 'set');
+			let value = e.target[attributeName];
+			value = isNumber ? Number(value) : value;
+			mutate(this.state, attributeValueSplit, value, 'set');
 		};
 
 		node.addEventListener(eventName, handler);
