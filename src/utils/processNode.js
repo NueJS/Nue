45c19cbca44d9c487c4b8bbbb145f22cad00e708
Manan Tank <manantankm@gmail.com>
2020-11-25 11:03:00 +0530

    add source in processing

--- src/utils/processNode.js
+++ src/utils/processNode.js
@@ -1,26 +1,26 @@
-function processNode (node) {
+function processNode (node, source = {}) {
   // no processing needed for style tag
   if (node.nodeName === 'STYLE') return
 
   // only process textContent in text node
   if (node.nodeName === '#text') {
-    this.processTextContent(node)
+    this.processTextContent(node, source)
     return
   }
 
   // process mapping on template with each attribute
   if (node.nodeName === 'TEMPLATE') {
-    this.processMapping(node)
+    this.processMapping(node, source)
     return
   }
   // process attributes for all other nodes
-  this.processAttributes(node)
+  this.processAttributes(node, source)
 
   // if node has multiple childNodes, do not process textContent on it
   if (node.childNodes.length > 1) {
-    [...node.childNodes].forEach(n => this.processNode(n))
+    [...node.childNodes].forEach(n => this.processNode(n, source))
   } else {
-    this.processTextContent(node)
+    this.processTextContent(node, source)
   }
 }
 
