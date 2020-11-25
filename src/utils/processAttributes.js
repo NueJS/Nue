e83f4bc31700b3b0aa58aa7b01e40584e19758b2
Manan Tank <manantankm@gmail.com>
2020-11-25 11:03:24 +0530

    add key for mapping

--- src/utils/processAttributes.js
+++ src/utils/processAttributes.js
@@ -1,7 +1,7 @@
 import getAttributes from './getAttributes.js'
 import { isCurled, uncurl } from './str.js'
 
-function processAttributes (node) {
+function processAttributes (node, source = {}) {
   const attributes = getAttributes(node)
 
   for (const attributeName in attributes) {
@@ -12,15 +12,20 @@ function processAttributes (node) {
     if (!isCurled(attributeValue)) continue
     attributeValue = uncurl(attributeValue)
 
+    if (attributeName === 'key') {
+      node.removeAttribute('key')
+      node.key = source[attributeValue]
+      return
+    }
+
     // if event handler attribute
     if (attributeName.startsWith('@')) {
       node.removeAttribute(attributeName) // remove @event={handler} from node
       const eventName = attributeName.substr(1) // get event from @event
-      const handler = this.options[attributeValue] // add event listener, bind the handler
-
+      const handler = this.options[attributeValue]
       node.addEventListener(eventName, handler.bind(this))
     } else {
-      this.bindAttributeValue(node, attributeName, attributeValue)
+      this.bindAttributeValue(node, attributeName, attributeValue, source)
       if (node.nodeName === 'INPUT') this.bindInput(node, attributeName, attributeValue)
     }
   }
