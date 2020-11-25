30635bf83b45c1dc531765ac64c6da9032caf283
Manan Tank <manantankm@gmail.com>
2020-11-25 11:03:35 +0530

    add deepClone

---
+++ src/utils/deepClone.js
@@ -0,0 +1,20 @@
+function deepClone (x) {
+  // value
+  if (typeof x !== 'object' || x === null) return x
+
+  // array
+  if (Array.isArray(x)) {
+    return x.map(v => deepClone(v))
+  }
+
+  // object
+  if (typeof x === 'object') {
+    const clone = {}
+    for (const key in x) {
+      clone[key] = deepClone(x[key])
+    }
+    return clone
+  }
+}
+
+export default deepClone
