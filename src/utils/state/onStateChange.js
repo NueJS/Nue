01eb5d244b3efbb137c0805b95279d657cbb8346
Manan Tank <manantankm@gmail.com>
2020-11-27 11:35:34 +0530

    new StateDeps structure

--- src/utils/state/onStateChange.js
+++ src/utils/state/onStateChange.js
@@ -1,7 +1,17 @@
-function onStateChange (k, cb) {
-  const key = k.substr(6) // remove state.
-  if (!this.stateDeps[key]) this.stateDeps[key] = []
-  this.stateDeps[key].push(cb)
+function onStateChange (chain, cb) {
+  let target = this.stateDeps
+  const lastIndex = chain.length - 2
+  chain.slice(1).forEach((c, i) => {
+    if (!target[c]) {
+      target[c] = {
+        $: []
+      }
+    }
+    target = target[c]
+    if (i === lastIndex) {
+      target.$.push(cb)
+    }
+  })
 }
 
 export default onStateChange
