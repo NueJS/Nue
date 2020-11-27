36a467b946de5eca2f9d0312b44a722f055a943e
Manan Tank <manantankm@gmail.com>
2020-11-27 17:43:09 +0530

    refactor mapping

--- src/utils/process/processMapping.js
+++ src/utils/process/processMapping.js
@@ -1,120 +1,118 @@
-import { getUncurledAttribute } from '../str.js'
+import { attr } from '../str.js'
 import deepClone from '../deepClone.js'
 import deepEqual from '../deepEqual.js'
 import processNode from './processNode.js'
 import onStateChange from '../state/onStateChange.js'
-import getValue from '../value.js'
+import getValue, { getSlice } from '../value.js'
 
-function processMapping (templateNode, context) {
-  const arrayKey = getUncurledAttribute(templateNode, 'each')
+function processMapping (template, context) {
+  const each = attr(template, 'each')
+  if (!each) return
+  const chain = each.split('.')
+  const as = attr.call(this, template, 'as')
+  const at = attr.call(this, template, 'at', true)
+  const key = attr.call(this, template, 'key', true)
+  // keep track of added nodes -> { key1: [...nodes], key2: [...nodes] }
+  const frags = {}
 
-  if (arrayKey) {
-    // get mapping attributes from template
-    const as = getUncurledAttribute(templateNode, 'as')
-    const at = getUncurledAttribute(templateNode, 'at')
-    const key = getUncurledAttribute(templateNode, 'key')
+  // create new frag and process frag children nodes
+  function createFrag (value, index) {
+    const frag = template.content.cloneNode(true)
+    const ctx = { ...context, [as]: value, [at]: index };
+    [...frag.children].forEach(n => processNode.call(this, n, ctx))
+    return frag
+  }
 
-    // keep track of added nodes
-    // { key1: [...nodes], key2: [...nodes] }
-    const frags = {}
+  // key is a unique property of each child in array
+  // if no unique property is given use the index
+  const getFragKey = (index) => key === null ? index : getSlice.call(this, chain)[key]
 
-    // keep copy of previous version of array
-    let prevArray, fromState
+  const saveFrag = (frag, fragKey) => {
+    frags[fragKey] = [...frag.children]
+  }
 
-    // create new frag and process frag children nodes
-    function createFrag (value, index) {
-      const frag = templateNode.content.cloneNode(true)
-      const ctx = { ...context, [as]: value, [at]: index };
-      [...frag.children].forEach(fragChild => processNode.call(this, fragChild, ctx))
-      return frag
-    }
+  const getArray = () => getValue.call(this, chain, context)[0]
 
-    // key is a unique property of each child in array
-    // if no unique property is given use the index
-    const getFragKey = (index) => key === null ? index : this.state[arrayKey][key]
+  const [array, isStateKey] = getValue.call(this, chain, context)
+  array.forEach((value, index) => {
+    const frag = createFrag.call(this, value, index)
+    const fragKey = getFragKey(index)
+    saveFrag(frag, fragKey)
+    template.before(frag)
+  })
 
-    const saveFrag = (frag, fragKey) => {
-      frags[fragKey] = [...frag.children]
-    }
+  const prevArray = deepClone(array)
 
-    function buildNodes () {
-      const [array, isStateKey] = getValue.call(this, arrayKey, context)
-      fromState = isStateKey
-      array.forEach((value, index) => {
-        const frag = createFrag.call(this, value, index)
-        const fragKey = getFragKey(index)
-        saveFrag(frag, fragKey)
-        templateNode.before(frag)
-      })
+  function reBuildNodes () {
+    const array = getArray()
+    const maxLength = Math.max(array.length, prevArray.length)
 
-      prevArray = deepClone(array)
-    }
+    for (let i = 0; i < maxLength; i++) {
+      const value = array[i]
+      // if ith value mismatch
+      if (!deepEqual(prevArray[i], value)) {
+        const fragKey = getFragKey(i)
 
-    function reBuildNodes () {
-      const [array] = getValue.call(this, arrayKey, context)
+        // if prevArray was shorter and new value is added in array
+        if (i > prevArray.length - 1) {
+          // create new node and add it at the end
+          const frag = createFrag.call(this, value, i)
+          saveFrag(frag, fragKey)
+          template.before(frag)
+          prevArray.push(deepClone(value))
+        } else {
+          // if the value is changed at index i
+          // change the frag nodes at index i
+          const oldNodes = frags[fragKey]
 
-      array.forEach((value, i) => {
-        // if ith value mismatch
-        if (!deepEqual(prevArray[i], value)) {
-          const fragKey = getFragKey(i)
-
-          // if prevArray was shorter and new value is added in array
-          if (i > prevArray.length - 1) {
-            // create new node and add it at the end
-            const frag = createFrag.call(this, value, i)
-            saveFrag(frag, fragKey)
-            templateNode.before(frag)
-            prevArray.push(deepClone(value))
-          } else {
-            // if the value is changed at index i
-            // change the frag nodes at index i
-            const oldNodes = frags[fragKey]
-
-            function updateNode (node) {
-              // debugger
-              if (node.mapArrayUsage) {
-                node.mapArrayUsage.forEach(usage => {
-                  const ctx = { ...context, [as]: value, [at]: i }
-                  // console.log({ context, usage, key: context[usage.key] })
-                  if (usage.type === 'text') {
-                    const currentValue = node.textContent
-                    const newValue = ctx[usage.key]
-                    if (currentValue !== newValue) { node.textContent = newValue }
-                  }
-                  else if (usage.type === 'attribute') {
-                    const currentValue = node.getAttribute(usage.name)
-                    const newValue = ctx[usage.key]
-                    if (currentValue !== newValue) { node.setAttribute(usage.name, newValue) }
-                  }
-                  else if (usage.type === 'state-attribute') {
-                    const currentValue = node.getAttribute(usage.name)
-                    const newValue = ctx[usage.key]
-                    if (currentValue !== newValue) { node.state[usage.name] = newValue }
-                  }
-                })
-              }
-              if (node.hasChildNodes) {
-                [...node.childNodes].forEach(n => updateNode(n))
-              }
+          function updateNode (node) {
+            // debugger
+            if (node.mapArrayUsage) {
+              node.mapArrayUsage.forEach(usage => {
+                const ctx = { ...context, [as]: value, [at]: i }
+                if (usage.type === 'text') {
+                  const currentValue = node.textContent
+                  const newValue = ctx[usage.key]
+                  if (currentValue !== newValue) { node.textContent = newValue }
+                }
+                else if (usage.type === 'attribute') {
+                  const currentValue = node.getAttribute(usage.name)
+                  const newValue = ctx[usage.key]
+                  if (currentValue !== newValue) { node.setAttribute(usage.name, newValue) }
+                }
+                else if (usage.type === 'state-attribute') {
+                  const currentValue = node.getAttribute(usage.name)
+                  const newValue = ctx[usage.key]
+                  if (currentValue !== newValue) { node.state[usage.name] = newValue }
+                }
+              })
             }
-
-            oldNodes.forEach((oldNode) => {
-              updateNode(oldNode)
-            })
-
-            prevArray[i] = deepClone(value)
+            if (node.hasChildNodes) {
+              [...node.childNodes].forEach(n => updateNode(n))
+            }
           }
-        }
-      })
-    }
 
-    // build nodes and when the array is changed, re-build
-    buildNodes.call(this)
-    if (fromState) {
-      onStateChange.call(this, arrayKey, () => {
-        reBuildNodes.call(this)
-      })
+          oldNodes.forEach((oldNode) => {
+            updateNode(oldNode)
+          })
+
+          prevArray[i] = deepClone(value)
+        }
+      }
     }
+  }
+
+  if (isStateKey) {
+    const l1 = chain.length
+    onStateChange.call(this, chain, (updatedChain) => {
+      const l2 = updatedChain.length
+      if (l2 > l1) {
+        reBuildNodes.call(this)
+      } else {
+        //
+        const additional = updatedChain.slice(l1 - 1)
+      }
+    })
   }
 }
 
