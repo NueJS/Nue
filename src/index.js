15e0b70558396d50154a10857963d9ab8db6da96
Manan Tank <manantankm@gmail.com>
2020-11-27 13:37:14 +0530

    dynamically add components

--- src/index.js
+++ src/index.js
@@ -1,20 +1,42 @@
 import addState from './utils/state/addState.js'
 import buildShadowDOM from './utils/buildShadowDOM.js'
 
-function $ (compName, compObj) {
+function loadComponents (template) {
+  [...template.content.children].forEach(node => {
+    checkNode(node)
+    if (node.children) {
+      [...node.children].forEach(n => checkNode(n))
+    }
+  })
+
+  function checkNode (node) {
+    const elementName = node.nodeName.toLowerCase()
+    const path = window.componentPaths[elementName]
+    if (path && !window.loadedComponents[elementName]) {
+      window.loadedComponents[elementName] = true
+      const script = document.createElement('script')
+      script.src = path
+      document.head.append(script)
+    }
+  }
+}
+
+function customElement (compName, compObj) {
   const template = document.createElement('template')
   const style = compObj.css ? `<style> ${compObj.css}</style>` : ''
   template.innerHTML = compObj.html.trim() + style
-
+  loadComponents(template)
   class El extends HTMLElement {
     constructor () {
       super()
-      this.stateDeps = { }
+      this.stateDeps = {}
       this.computedStateDeps = []
       this.compObj = compObj
       this.refs = {}
+      this.compName = compName
       addState.call(this)
       buildShadowDOM.call(this, template)
+      window.loadedComponents[compName] = true
     }
 
     connectedCallback () {
@@ -29,4 +51,15 @@ function $ (compName, compObj) {
   customElements.define(compName, El)
 }
 
-export default $
+window.customElement = customElement
+window.loadedComponents = {
+
+}
+
+window.showNodeUpdates = true
+window.textNodeUpdated = (textNode) => {
+  textNode.parentNode.style.background = 'yellow'
+  setTimeout(() => {
+    textNode.parentNode.style.background = null
+  }, 300)
+}
