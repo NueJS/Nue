fd0981cbc1637d72633ae0df6b7b6c26154e4d2a
Manan Tank <manantankm@gmail.com>
2020-12-01 00:21:30 +0530

    add refs API

--- src/utils/process/processAttributes.js
+++ src/utils/process/processAttributes.js
@@ -4,16 +4,19 @@ import bindAttribute from '../bind/bindAttribute.js'
 import bindState from '../bind/bindState.js'
 
 function processAttributes (node, context) {
+  const ref = node.getAttribute('ref')
+  if (ref) {
+    this.config.refs[ref] = node
+  }
+
   const info = this.getNodeInfo(node)
   if (!(info && info.attributes)) return
 
   info.attributes.forEach(attribute => {
-    if (attribute.eventName) { // @xyz={abc}
-      if (attribute.handler) { // @click={handler}
-        node.addEventListener(attribute.eventName, this.handle[attribute.handler])
-      } else if (attribute.targetProp) { // @input:value={state.key}
-        bindInput.call(this, node, attribute.eventName, attribute.targetProp, attribute.stateChain)
-      }
+    if (attribute.eventName) { // @eventName={handler}
+      node.addEventListener(attribute.eventName, this.handle[attribute.handler])
+    } else if (attribute.bindProp) { // bind:bindProp={state.key}
+      bindInput.call(this, node, attribute.bindProp, attribute.stateChain)
     } else if (attribute.propName) { // :name={var} or :name=value
       bindState.call(this, node, attribute.propName, attribute.isVar, attribute.stateChain)
     } else {
