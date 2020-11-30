16d465d9566c11a048b602bbf23a6f05f6053d37
Manan Tank <manantankm@gmail.com>
2020-12-01 00:22:57 +0530

    bring back the old bind: attribute

--- src/utils/bind/bindInput.js
+++ src/utils/bind/bindInput.js
@@ -3,20 +3,21 @@ import onStateChange from '../reactivity/onStateChange.js'
 import getSlice from '../value.js'
 
 // @eventName:prop={stateChain}
-function bindInput (node, eventName, prop, stateChain) {
+function bindInput (node, bindProp, stateChain) {
+  console.log({ node, bindProp, stateChain })
   const isNumber = node.type === 'number' || node.type === 'range'
-  node[prop] = getSlice(this.state, stateChain)
+  node[bindProp] = getSlice(this.state, stateChain)
 
   const handler = e => {
-    const value = e.target[prop]
+    const value = e.target[bindProp]
     mutate(this.state, stateChain, isNumber ? Number(value) : value, 'set')
   }
 
   onStateChange.call(this, stateChain, () => {
-    node[prop] = getSlice(this.state, stateChain)
+    node[bindProp] = getSlice(this.state, stateChain)
   })
 
-  node.addEventListener(eventName, handler)
+  node.addEventListener('input', handler)
 }
 
 export default bindInput
