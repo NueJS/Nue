8ee6691c8f318311bc9b77b5761dcd07b59e5e5e
Manan Tank <manantankm@gmail.com>
2020-12-01 00:22:31 +0530

    add reverseForeach in others.js

--- src/utils/others.js
+++ src/utils/others.js
@@ -1,3 +1,14 @@
 
 let i = 0
-export const uid = () => i++
+export const uid = () => '' + i++
+
+/**
+ *
+ * @param {Array} arr
+ * @param {Function} cb
+ */
+export function reverseForEach (arr, cb) {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    cb(arr[i], i)
+  }
+}
