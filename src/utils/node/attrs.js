3c94a41d5407430de155371ba2a0dea10a5522e0
Manan Tank <manantankm@gmail.com>
2020-12-01 00:18:35 +0530

    move attrs.js to node folder

--- src/utils/node/attrs.js
+++ src/utils/node/attrs.js
@@ -1,8 +1,10 @@
-import { isCurled, uncurl } from './str.js'
+import { isCurled, uncurl } from '../str.js'
 
-// returns an object of arrays
-// example
-// { id: ['cool', false], ':count': ['state.count', true]}
+/**
+ * returns an object of arrays which shows name, value and isVar property of an attribute
+ * @param {Element} node - node for which attributes are to be returned
+ * @returns {Object} - { id: ['cool', false], ':count': ['state.count', true]}
+ */
 
 function attrs (node) {
   const attributes = {}
