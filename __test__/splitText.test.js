007fefcd5737a5d7468deb0b6abd5394ed7aa566
Manan Tank <manantankm@gmail.com>
2020-12-01 14:56:43 +0530

    change state to $

--- __test__/splitText.test.js
+++ __test__/splitText.test.js
@@ -1,39 +1,39 @@
 import { splitText } from '../src/utils/str.js'
 
 test('splitText detects variable at the start of the string', () => {
-  const parts = splitText('{state.a.b}abcdefghi')
+  const parts = splitText('{$.a.b}abcdefghi')
   expect(parts).toEqual([
-    { string: 'state.a.b', isVariable: true },
+    { string: '$.a.b', isVariable: true },
     { string: 'abcdefghi' }
   ])
 })
 
 test('splitText detects variable at the end of the string', () => {
-  const parts = splitText('abcdefghi{state.a.b}')
+  const parts = splitText('abcdefghi{$.a.b}')
   expect(parts).toEqual([
     { string: 'abcdefghi' },
-    { string: 'state.a.b', isVariable: true }
+    { string: '$.a.b', isVariable: true }
   ])
 })
 
 test('splitText detects variable at the middle of the string', () => {
-  const parts = splitText('abcdefghi{state.a.b}abcdefghi')
+  const parts = splitText('abcdefghi{$.a.b}abcdefghi')
   expect(parts).toEqual([
     { string: 'abcdefghi' },
-    { string: 'state.a.b', isVariable: true },
+    { string: '$.a.b', isVariable: true },
     { string: 'abcdefghi' }
   ])
 })
 
 test('splitText detects multiple variables', () => {
-  const parts = splitText('xx{state.a.b}xx{state.c.d}xx{state.e.f}xx')
+  const parts = splitText('xx{$.a.b}xx{$.c.d}xx{$.e.f}xx')
   expect(parts).toEqual([
     { string: 'xx' },
-    { string: 'state.a.b', isVariable: true },
+    { string: '$.a.b', isVariable: true },
     { string: 'xx' },
-    { string: 'state.c.d', isVariable: true },
+    { string: '$.c.d', isVariable: true },
     { string: 'xx' },
-    { string: 'state.e.f', isVariable: true },
+    { string: '$.e.f', isVariable: true },
     { string: 'xx' }
   ])
 })
