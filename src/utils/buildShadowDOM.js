8490c700d0dc3b0eb83dfdc41bd4bca2e7da3ad9
Manan Tank <manantankm@gmail.com>
2020-12-01 14:54:55 +0530

    fix buildShadowDOM

--- src/utils/buildShadowDOM.js
+++ src/utils/buildShadowDOM.js
@@ -2,10 +2,9 @@ import processNode from './process/processNode.js'
 
 function buildShadowDOM (template) {
   this.attachShadow({ mode: this.mode })
-  const fragment = template.content.cloneNode(true)
-  const fragmentNodes = [...fragment.childNodes]
-  fragmentNodes.forEach(node => this.shadowRoot.append(node))
-  fragmentNodes.forEach(node => processNode.call(this, node))
+  const fragment = template.content.cloneNode(true);
+  [...fragment.childNodes].forEach(node => processNode.call(this, node));
+  [...fragment.childNodes].forEach(node => this.shadowRoot.append(node))
 }
 
 export default buildShadowDOM
