7c365bbe2151afb1f6a8da303734f2c9600236bd
Manan Tank <manantankm@gmail.com>
2020-12-01 14:52:00 +0530

    bind props

--- src/utils/bind/bindState.js
+++ src/utils/bind/bindState.js
@@ -2,29 +2,50 @@ import getSlice from '../value.js'
 import onStateChange from '../reactivity/onStateChange.js'
 import { mutate } from '../reactivity/mutate.js'
 
-function addProps (node, key, value) {
+function addProps (node, key, value, onNodeStateChange) {
   if (!node.props) node.props = {}
   node.props[key] = value
+  if (onNodeStateChange) {
+    if (!node.twoWayProps) node.twoWayProps = []
+    node.twoWayProps.push(onNodeStateChange)
+  }
 }
 
-function bindState (node, propName, isVar, stateChain) {
+function bindState (node, propName, isVar, stateChain, isTwoWayBind) {
   const propNameSplit = propName.split('.')
-  if (isVar) {
-    addProps(node, name, getSlice(this.state, stateChain))
+  console.log({ propName, isVar, stateChain })
+  let onNodeStateChange
 
-    // change node's state when its props change
+  if (isVar) {
+    if (isTwoWayBind) {
+      onNodeStateChange = () => {
+        onStateChange.call(node, propNameSplit, () => {
+          const value = getSlice(node.$, propNameSplit)
+          // to avoid infinite loop
+          // disable state change in child which triggers the change in parent
+          node.ignoreStateChange = propNameSplit[0]
+          mutate(this.$, stateChain, value, 'set')
+          node.ignoreStateChange = undefined
+        })
+      }
+    }
+    addProps(node, propName, getSlice(this.$, stateChain), onNodeStateChange)
+
+    // change child nodes' state when parent node's state changes
     onStateChange.call(this, stateChain, () => {
-      mutate(node.state, propNameSplit, getSlice(this.state, stateChain), 'set')
+      mutate(node.$, propNameSplit, getSlice(this.$, stateChain), 'set')
     })
 
     // else {
     //   addProps(node, name, key)
     //   addContextDependency(node, {
-    //     type: 'state-attribute',
+    //     type: '$-attribute',
     //     name: name,
     //     key
     //   })
     // }
+  } else {
+    addProps(node, propName, stateChain)
   }
 }
 
--- src/utils/reactivity/onChange.js
+++ src/utils/reactivity/onChange.js
@@ -2,16 +2,20 @@ import { silentMutate } from './mutate.js'
 import { call$Cbs, callAllCbs } from './cbs.js'
 
 function onChange (chain, value, trap) {
-  const success = silentMutate(this.state, chain, value, trap)
+  if (this.ignoreStateChange === chain[0]) return true
+  const success = silentMutate(this.$, chain, value, trap)
   // ignore .length updates
   if (chain[chain.length - 1] === 'length') return success
 
-  // call cbs which deps on any change in state
+  // console.log({ chain })
+
+  // call cbs which deps on any change in $
   this.stateDeps.$.forEach(cb => cb())
 
   // call cbs which need to be called
   let target = this.stateDeps
   chain.forEach((c, i) => {
+    if (typeof target !== 'object') return
     target = target[c]
     if (target) {
       if (i !== chain.length - 1) call$Cbs(target, chain)
