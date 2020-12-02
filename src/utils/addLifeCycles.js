32eb96e1895a33298cf0d352c3c5ce5008550a89
Manan Tank <manantankm@gmail.com>
2020-12-02 11:58:57 +0530

    beforeUpdate and afterUpdate lifecycle hooks added

--- src/utils/addLifeCycles.js
+++ src/utils/addLifeCycles.js
@@ -1,19 +1,20 @@
 import onStateChange from './reactivity/onStateChange.js'
 
+function update (fn, deps, type) {
+  if (!deps) this.stateDeps.$.push(fn)
+  else {
+    deps.forEach(d => {
+      onStateChange.call(this, d.split('.'), fn, type)
+    })
+  }
+}
+
 function addLifeCycles () {
   this.on = {
     add: (fn) => this.onAddCbs.push(fn),
     remove: (fn) => this.onRemoveCbs.push(fn),
-    update: (fn, deps) => {
-      if (!deps) {
-        this.stateDeps.$.push(fn)
-      }
-      else {
-        deps.forEach(d => {
-          onStateChange.call(this, d.split('.'), fn)
-        })
-      }
-    }
+    beforeUpdate: (fn, deps) => update.call(this, fn, deps, 'before'),
+    afterUpdate: (fn, deps) => update.call(this, fn, deps, 'after', this)
   }
 }
 
