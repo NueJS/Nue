aa8949a4dbb37a2e3a7d946ba10f86710c7d460c
Manan Tank <manantankm@gmail.com>
2020-12-03 22:35:23 +0530

    rename config to settings

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+import onNodeUpdate from './devtools/onNodeUpdate.js'
+
+const settings = {
+  showUpdates: false,
+
+  // callback function that should be called when a node is updated in DOM
+  // this function is called if showUpdates in true
+  // in this function you can define your own logic to create a devtool to show DOM updates
+
+  // this function will usually be called by a textNode
+  // you can not show highlights on a text node, so target its parent instead
+  onNodeUpdate,
+  commonCSS: ':host {display: block;}'
+}
+
+export default settings
