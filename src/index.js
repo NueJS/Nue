49cfa41493d0c3a153ecc181566961d4dd125b64
Manan Tank <manantankm@gmail.com>
2020-12-03 22:30:30 +0530

    add components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import defineComponent from './defineComponent.js'
 
+// define all the components
+
+// @TODO : do not define components all at once
+// start from root - when processing template of app
+// go through nodes, if a custom component is used, process that first
+// do the same in that component
+
+// if a component is hidden, do not process it
+// if the component becomes unhidden check if has been processed or not
+// if not, process it
+
 export const components = (comps) => {
   Object.keys(comps).forEach(name => defineComponent(name, comps[name]))
 }
