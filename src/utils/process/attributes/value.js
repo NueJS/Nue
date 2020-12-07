12ee816faeec732eecd70f246194c69ad7739e24
Manan Tank <manantankm@gmail.com>
2020-12-07 23:29:35 +0530

    update proces_attribtues

--- src/utils/process/attributes/value.js
+++ src/utils/process/attributes/value.js
@@ -5,27 +5,21 @@ import { FN, REACTIVE } from '../../constants.js'
 // get the value of slice having the given path and set the attribute value
 // when the slice changes, update the value of attribute as well
 function process_attribute (node, info) {
-  const { placeholder, name, type } = info
-  // console.log({ name, nodeName: node.nodeName })
-  if (info.placeholder.type === FN) {
-    const { on_args_change, deps } = info.placeholder
-    const update_attribute = on_args_change((value) => {
-      node.setAttribute(name, value)
-    })
-    update_attribute()
-    this.on.domUpdate(update_attribute, deps)
+  const { placeholder, name } = info
+  let get_value, dependency
+
+  if (placeholder.type === FN) {
+    get_value = () => placeholder.get_value()
+    dependency = placeholder.deps
+  } else if (placeholder.type === REACTIVE) {
+    get_value = () => slice(this.$, placeholder.path)
+    dependency = [placeholder.content]
   }
 
-  else if (info.placeholder.type === REACTIVE) {
-    const update_attribute = () => {
-      console.log({ placeholder })
-      const value = slice(this.$, placeholder.path)
-      node.setAttribute(name, value)
-    }
+  const set_value = () => node.setAttribute(name, get_value())
 
-    update_attribute()
-    add_slice_dependency.call(this, placeholder.path, update_attribute)
-  }
+  set_value()
+  this.on.domUpdate(set_value, dependency)
 }
 
 export default process_attribute
