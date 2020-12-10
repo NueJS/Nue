64fc0e8bac90b5e3c4561b4ffd274f759cad1540
Manan Tank <manantankm@gmail.com>
2020-12-10 09:50:00 +0530

    new text node processing

--- src/utils/process/text_node.js
+++ src/utils/process/text_node.js
@@ -1,71 +1,46 @@
 import slice from '../slice/slice.js'
-import add_slice_dependency from '../slice/add_slice_dependency.js'
-import { memoize_cb } from '../callbacks.js'
 import settings from '../../settings.js'
-import { FN, REACTIVE, TEXT } from '../constants.js'
+import { FN, REACTIVE } from '../constants.js'
 import { add_connects } from '../node/connections.js'
 
 // @todo move this processing step from process node to in process template
 function process_text_node (text_node) {
-  const parts = this.memo_of(text_node).parts
-  const text_nodes = []
-  let prev_node_is_text_node = true
+  const placeholder = this.memo_of(text_node)
+  // if (!placeholder) return // when can this happen ?
 
-  // if the previous node is text node
-  // join the string - don't create a new text node
-  const add_text_node = (text) => {
-    if (text_nodes.length && prev_node_is_text_node) {
-      const prev_node = text_nodes[text_nodes.length - 1]
-      prev_node.textContent += text
-    } else {
-      text_nodes.push(document.createTextNode(text))
+  const { type, get_value, path, text, deps } = placeholder
+  let update
+
+  if (type === REACTIVE) {
+    let value
+    try { value = get_value() } catch { /**/ }
+    // forgive errors - if not a valid placeholder - turn it back to string
+    if (value === undefined) {
+      text_node.textContent = '[' + text + ']'
+      return // do not connect to state
+    }
+
+    else {
+      text_node.textContent = value
+      update = () => {
+        if (!text_node.connected_to_state) return
+        text_node.textContent = slice(this.$, path)
+        if (settings.showUpdates) settings.onNodeUpdate(text_node)
+      }
     }
   }
 
-  parts.forEach(part => {
-    const { type, get_value, path, text, content, deps } = part
-
-    if (type === TEXT) add_text_node(text)
-
-    else if (type === REACTIVE) {
-      let value
-      try { value = get_value() } catch { /**/ }
-      if (value === undefined) add_text_node('[' + content + ']')
-      else {
-        const textNode = document.createTextNode(value)
-
-        const cb = () => {
-          textNode.textContent = slice(this.$, path)
-          if (settings.showUpdates) settings.onNodeUpdate(textNode)
-        }
-
-        add_connects(textNode, () => this.on.domUpdate(cb, deps))
-        text_nodes.push(textNode)
-        prev_node_is_text_node = false
-      }
+  else if (type === FN) {
+    text_node.textContent = get_value()
+    update = () => {
+      if (settings.showUpdates) settings.onNodeUpdate(text_node)
+      text_node.textContent = get_value()
     }
-
-    else if (part.type === FN) {
-      prev_node_is_text_node = false
-      const node = document.createTextNode('')
-      const update = () => {
-        if (settings.showUpdates) settings.onNodeUpdate(node)
-        node.textContent = part.get_value()
-      }
-      update()
-      add_connects(node, () => this.on.domUpdate(update, deps))
-      text_nodes.push(node)
-    }
-  })
-
-  // if node is text content, replace new text nodes with this current one
-  if (text_node.nodeType === Node.TEXT_NODE) {
-    text_nodes.forEach(n => text_node.before(n))
-    text_node.remove()
-  } else {
-    text_node.innerHTML = ''
-    text_nodes.forEach(n => text_node.append(n))
   }
+
+  const connect = () => this.on.domUpdate(update, ...deps)
+  add_connects(text_node, connect)
+  text_node.update_value = update
 }
 
 export default process_text_node
