c77448389ecddcc788ecf0089e819ac50fff3058
Manan Tank <manantankm@gmail.com>
2020-12-10 09:41:17 +0530

    perform delayed processes after all nodes are processed

--- src/utils/build_shadow_dom.js
+++ src/utils/build_shadow_dom.js
@@ -4,6 +4,7 @@ import traverse from './tree/traverse.js'
 const add_memo_ids = (fragment) => {
   let memo_id = 0
   traverse(fragment, (node) => {
+    // if (node.nodeName === '')
     node.memo_id = ++memo_id
   }, true)
 }
@@ -12,7 +13,8 @@ function build_shadow_dom (template) {
   this.attachShadow({ mode: this.memo.mode })
   const fragment = template.content.cloneNode(true)
   add_memo_ids(fragment)
-  process_node.call(this, fragment);
+  process_node.call(this, fragment)
+  this.delayed_processes.forEach(p => p());
   [...fragment.childNodes].forEach(node => this.shadowRoot.append(node))
 }
 
