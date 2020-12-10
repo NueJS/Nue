027862d87f8d89fcb4a32def07d4c894c8ba78ba
Manan Tank <manantankm@gmail.com>
2020-12-10 09:43:26 +0530

    delay the replacement of node with text_nodes

--- src/utils/memoize/text-content.js
+++ src/utils/memoize/text-content.js
@@ -1,9 +1,30 @@
+import { TEXT } from '../constants.js'
 import split from '../string/split.js'
 
-function memoize_text_content (node, memo_id) {
-  this.memo.nodes[memo_id] = {}
-  const parts = split.call(this, node.textContent)
-  this.memo.nodes[memo_id].parts = parts
+function memoize_text_content (node) {
+  const placeholders = split.call(this, node.textContent.trim())
+  const textNodes = []
+
+  placeholders.forEach(placeholder => {
+    this.memo_id++
+    let textNode
+
+    if (placeholder.type === TEXT) {
+      textNode = document.createTextNode(placeholder.text)
+    } else {
+      // check if the node is empty string, remove it then
+      this.memo.nodes[this.memo_id] = placeholder
+      textNode = document.createTextNode('')
+    }
+
+    textNodes.push(textNode)
+  })
+
+  // after all memoization is done, replace the node with textNodes
+  this.delayed_memoizations.push(() => {
+    textNodes.forEach(t => node.before(t))
+    node.remove()
+  })
 }
 
 export default memoize_text_content
