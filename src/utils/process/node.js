2ce0b04c84af51f5930168c27e54c6f281d3db9a
Manan Tank <manantankm@gmail.com>
2020-12-10 09:37:49 +0530

    wrap memo dependent processors with condition

--- src/utils/process/node.js
+++ src/utils/process/node.js
@@ -1,20 +1,18 @@
 import process_text_node from './text_node.js'
 import process_attributes from './attributes/all.js'
-// import process_condition from './condition.js'
-// import process_map from './map.js'
-import process_if from './if.js'
+import process_if from './conditional_rendering/if.js'
 
-// m_id is just for the conditional and mapping
-// when cloning the node, ids are removed from the node
-// that's why
 function process_node (node, context) {
   if (node.nodeType !== Node.DOCUMENT_FRAGMENT_NODE) {
+    // console.log(node.nodeName, node.memo_id)
     if (node.processed) return
     node.processed = true
 
-    if (node.nodeType === Node.TEXT_NODE) process_text_node.call(this, node, context)
-    else if (node.nodeName === 'IF') process_if.call(this, node)
-    else process_attributes.call(this, node, context)
+    if (this.memo_of(node)) {
+      if (node.nodeType === Node.TEXT_NODE) process_text_node.call(this, node, context)
+      else if (node.nodeName === 'IF') process_if.call(this, node)
+      else if (node.hasAttribute) process_attributes.call(this, node, context)
+    }
   }
 
   if (node.hasChildNodes()) {
