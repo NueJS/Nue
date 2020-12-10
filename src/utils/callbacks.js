7e938ea65a8544f22fdadd6224811896947ba911
Manan Tank <manantankm@gmail.com>
2020-12-10 09:38:55 +0530

    call the callbacks with old value, not chain

--- src/utils/callbacks.js
+++ src/utils/callbacks.js
@@ -1,31 +1,33 @@
+// import slice from './slice/slice.js'
+
 // call only the cbs in $ array in target
-function call_cb_map (map, chain) {
+function call_cb_map (map, info) {
   for (const [cb] of map) {
-    cb.call(this, chain)
+    cb.call(this, info)
   }
 }
 
-export function call_$_cbs (target, chain) {
+export function call_$_cbs (target, info) {
   const { reactive, dom, before, after } = target.$
-  call_cb_map.call(this, reactive, chain)
-  call_cb_map.call(this, before, chain)
-  call_cb_map.call(this, dom, chain)
-  call_cb_map.call(this, after, chain)
+  call_cb_map.call(this, reactive, info)
+  call_cb_map.call(this, before, info)
+  call_cb_map.call(this, dom, info)
+  call_cb_map.call(this, after, info)
 }
 
-export function call_all_cbs (target, chain) {
+export function call_all_cbs (target, info) {
   for (const k in target) {
-    if (k === '$') call_$_cbs.call(this, target, chain)
-    else call_all_cbs.call(this, target[k])
+    if (k === '$') call_$_cbs.call(this, target, info)
+    else call_all_cbs.call(this, target[k], info)
   }
 }
 
 // convert consecutive calls to single call
-// instead of using a flag, function is saved in registered_callbacks
+// instead of using a flag, function is saved in queue
 // because it should be called once batching is completed
 export function memoize_cb (fn, type) {
-  const lifecycle = this.registered_callbacks[type]
+  const lifecycle = this.queue[type]
   return (...args) => {
-    if (!lifecycle.has(fn)) lifecycle.set(fn, args)
+    if (!lifecycle.has(fn)) lifecycle.set(fn, ...args)
   }
 }
