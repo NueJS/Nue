ebfd9ae2ceb8a917ce20c117e4aaca01ad8ec7d7
Manan Tank <manantankm@gmail.com>
2020-12-11 13:41:37 +0530

    add when.component hooks

--- src/utils/add_lifecycle_hooks.js
+++ src/utils/add_lifecycle_hooks.js
@@ -1,28 +1,29 @@
-import add_slice_dependency from './slice/add_slice_dependency.js'
-import { memoize_cb } from './callbacks.js'
+// import add_state_dep from './slice/add_state_dep.js'
+// import { memoize_cb } from './callbacks.js'
 // import { update_type } from './constants.js'
 
 function add_lifecycle_hooks () {
-  this.on = {
-    add: (fn) => this.add_callbacks.push(fn),
-    reactiveUpdate: (fn, ...deps) => update.call(this, fn, deps, 'reactive'),
-    beforeUpdate: (fn, ...deps) => update.call(this, fn, deps, 'before'),
-    domUpdate: (fn, ...deps) => update.call(this, fn, deps, 'dom'),
-    afterUpdate: (fn, ...deps) => update.call(this, fn, deps, 'after'),
-    remove: (fn) => this.remove_callbacks.push(fn)
+  this.when = {
+    component: {
+      isAdded: (cb) => this.is_added_cbs.push(cb),
+      isRemoved: (cb) => this.is_removed_cbs.push(cb),
+      willUpdate: (cb) => this.will_update_cbs.push(cb),
+      isUpdated: (cb) => this.is_updated_cbs.push(cb)
+    }
+    // $: this.$
   }
 }
 
-function update (fn, deps, type) {
-  // // console.log({ deps })
-  // fn.lifecycle = type
-  if (!deps) throw new Error(`No Dependency array given in ${fn.name}`)
-  else {
-    // batch if not reactive update
-    const cb = (type !== 'reactive') ? memoize_cb.call(this, fn, type) : fn
-    // return array of cleanups
-    return deps.map(d => add_slice_dependency.call(this, d.split('.'), cb, type))
-  }
-}
+// function update (cb, deps, type) {
+//   // // console.log({ deps })
+//   // cb.lifecycle = type
+//   if (!deps) throw new Error(`No Dependency array given in ${cb.name}`)
+//   else {
+//     // batch if not reactive update
+//     const cb = (type !== 'reactive') ? memoize_cb.call(this, cb, type) : cb
+//     // return array of cleanups
+//     return deps.map(d => add_state_dep.call(this, d.split('.'), cb, type))
+//   }
+// }
 
 export default add_lifecycle_hooks
