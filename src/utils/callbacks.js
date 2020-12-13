e117f03f17b1d562b415b615c535339478f2a223
Manan Tank <manantankm@gmail.com>
2020-12-13 16:15:46 +0530

    all deps are treated equally no matter what lifecycle it is for

--- src/utils/callbacks.js
+++ src/utils/callbacks.js
@@ -1,33 +1,44 @@
-// import slice from './slice/slice.js'
 
-// call only the cbs in $ array in target
-function call_cb_map (map, info) {
-  for (const [cb] of map) {
+export function triggerMapCbs (map) {
+  for (const [fn, args] of map) {
+    fn(args)
+  }
+}
+
+// only trigger cbs in $ Map
+export function trigger$Cbs (target, info) {
+  for (const [cb] of target.$) {
     cb.call(this, info)
   }
 }
 
-export function call_$_cbs (target, info) {
-  const { reactive, dom, before, after } = target.$
-  call_cb_map.call(this, reactive, info)
-  call_cb_map.call(this, before, info)
-  call_cb_map.call(this, dom, info)
-  call_cb_map.call(this, after, info)
-}
-
-export function call_all_cbs (target, info) {
+// trigger all the cbs in the object
+export function triggerAllCbs (target, info) {
   for (const k in target) {
-    if (k === '$') call_$_cbs.call(this, target, info)
-    else call_all_cbs.call(this, target[k], info)
+    if (k === '$') trigger$Cbs.call(this, target, info)
+    else triggerAllCbs.call(this, target[k], info)
   }
 }
 
 // convert consecutive calls to single call
 // instead of using a flag, function is saved in queue
 // because it should be called once batching is completed
-export function memoize_cb (fn, type) {
+export function cbQueuer (fn, type) {
   const lifecycle = this.queue[type]
   return (...args) => {
     if (!lifecycle.has(fn)) lifecycle.set(fn, ...args)
   }
+}
+
+// find deps that needs to be called for given path update and trigger them
+export function triggerDeps (path, info) {
+  let target = this.deps
+  path.forEach((c, i) => {
+    if (typeof target !== 'object') return
+    target = target[c]
+    if (target) {
+      if (i !== path.length - 1) trigger$Cbs.call(this, target, info)
+      triggerAllCbs.call(this, target, info)
+    }
+  })
 }
