757466985b7186ca556b317cafe83f03ac396774
Manan Tank <manantankm@gmail.com>
2020-12-13 16:29:42 +0530

    combined FN and reactive placeholder updates

--- src/utils/process/attributes/name.js
+++ src/utils/process/attributes/name.js
@@ -1,24 +1,13 @@
-import slice from '../../slice/slice.js'
-// import add_slice_dependency from '../../slice/add_slice_dependency.js'
-import { FN, REACTIVE } from '../../constants.js'
-import { add_connects } from '../../node/connections.js'
+import { addConnects } from '../../node/connections.js'
+import addDep from '../../slice/addDep.js'
 
 // get the value of slice having the given path and set the attribute value
 // when the slice changes, update the value of attribute as well
 function process_attribute (node, info) {
   const { placeholder, name } = info
-  let get_value, dependency
-
-  if (placeholder.type === FN) {
-    get_value = () => placeholder.get_value()
-    dependency = placeholder.deps
-  } else if (placeholder.type === REACTIVE) {
-    get_value = () => slice(this.$, placeholder.path)
-    dependency = [placeholder.content]
-  }
-
-  const set_value = () => node.setAttribute(name, get_value())
-  add_connects(node, () => this.on.domUpdate(set_value, ...dependency))
+  const set_value = () => node.setAttribute(name, placeholder.get_value())
+  const connect = () => placeholder.deps.map(path => addDep.call(this, path, set_value, 'dom'))
+  addConnects(node, connect)
 }
 
 export default process_attribute
