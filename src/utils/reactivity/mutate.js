88746af6af4c82447edc63093aed9dfdcf66ccec
Manan Tank <manantankm@gmail.com>
2020-12-13 19:49:45 +0530

    use reduce in mutate

--- src/utils/reactivity/mutate.js
+++ src/utils/reactivity/mutate.js
@@ -1,5 +1,3 @@
-// import modes from './modes.js'
-
 // mutate the object using path to find target,
 // value to to change the value on target
 // and trap to define the kind of change
@@ -8,9 +6,8 @@
 // to do -> obj.x.y.z = 100, call mutate like this:
 // mutate(obj, ['x', 'y', 'z'], 100, 'set')
 
-export const mutate = ($, path, value, trap) => {
-  let target = $
-  path.slice(0, -1).forEach((key) => (target = target[key]))
+export const mutate = (obj, path, value, trap) => {
+  const target = path.slice(0, -1).reduce((acc, key) => acc[key], obj)
   const prop = path[path.length - 1]
   return Reflect[trap](target, prop, value)
 }
