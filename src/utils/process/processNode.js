edfa9b1b0ee52d5b2ef76d1319a306c0f4058d83
Manan Tank <manantankm@gmail.com>
2020-12-14 13:02:12 +0530

    render sweet components when processing

--- src/utils/process/processNode.js
+++ src/utils/process/processNode.js
@@ -1,6 +1,7 @@
 import processTextNode from './textNode.js'
 import process_attributes from './attributes/processAttributes.js'
 import process_if from './if/if.js'
+import { render, supersweet } from '../../index.js'
 
 function processNode (node, context) {
   if (node.nodeType !== Node.DOCUMENT_FRAGMENT_NODE) {
@@ -10,6 +11,10 @@ function processNode (node, context) {
     if (node.sweet.placeholder && node.nodeType === Node.TEXT_NODE) processTextNode.call(this, node)
     else if (node.nodeName === 'IF') process_if.call(this, node)
     else if (node.hasAttribute) process_attributes.call(this, node)
+
+    if (node.sweet.isSweet && !supersweet.processedComponents[node.sweet.compName]) {
+      render(node.sweet.compName)
+    }
   }
 
   if (node.hasChildNodes()) {
