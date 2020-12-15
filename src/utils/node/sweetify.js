05814d7941ce926ec5a0b4dd73912c38d771671f
Manan Tank <manantankm@gmail.com>
2020-12-15 10:10:57 +0530

    use shallow copy in sweetify

--- src/utils/node/sweetify.js
+++ src/utils/node/sweetify.js
@@ -2,7 +2,8 @@
 // cloneNode is clone of node but it does not have custom .sweet properties added in node's tree
 // sweetify is used to add those properties back in the cloneNode
 const sweetify = (node, cloneNode) => {
-  if (node.sweet) cloneNode.sweet = node.sweet
+  // do not give the ref directly, make a shallow clone
+  if (node.sweet) cloneNode.sweet = { ...node.sweet }
   if (node.hasChildNodes()) {
     node.childNodes.forEach((n, i) => {
       sweetify(n, cloneNode.childNodes[i])
