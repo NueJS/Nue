c64b98486000cedfbb2b19468f1696dc64266e30
Manan Tank <manantankm@gmail.com>
2020-12-16 21:49:16 +0530

    update process For

--- src/utils/process/for/processFor.js
+++ src/utils/process/for/processFor.js
@@ -1,14 +1,19 @@
-import sweetify from '../../node/sweetify.js'
-import traverse from '../../node/traverse.js'
+// import sweetify from '../../node/sweetify.js'
+// import traverse from '../../node/traverse.js'
 import addDep from '../../state/addDep.js'
-import processNode from '../processNode.js'
-import slice from '../../state/slice.js'
+// import processNode from '../processNode.js'
+// import slice from '../../state/slice.js'
+import { render, supersweet } from '../../../index.js'
+// import { STATE } from '../../constants.js'
 
 function processFor (forNode) {
   const forNodes = []
   const groups = []
 
   let $each, $of, $at, $hash
+
+  if (!forNode.hasAttribute('name')) throw new Error('for loop must have a name')
+  const name = forNode.getAttribute('name') + '-'
 
   forNode.sweet.attributes.forEach(attribute => {
     if (attribute.name === 'each') $each = attribute.placeholder
@@ -17,62 +22,32 @@ function processFor (forNode) {
     else if (attribute.name === 'at') $at = attribute.placeholder
   })
 
-  // collect all the forNodes inside the for node and mark them processed
-  forNode.childNodes.forEach(node => {
-    if (node.sweet) node.sweet.isProcessed = true
-    forNodes.push(node)
-  })
+  function loopComponent ({ html, on }) {
+    on.destroy(() => console.log('loop component removed'))
+    html(forNode.innerHTML)
+  }
+
+  supersweet.components[name] = loopComponent
 
   const renderLoop = () => {
-    // get the value of array
     const array = $of.getValue.call(this, forNode)
 
-    // loop over array and get the value and indexes
     array.forEach((value, i) => {
-      const context = {
+      const loopComponentInstance = document.createElement(name)
+
+      loopComponentInstance.stateProps = {
         [$each.content]: value,
         [$at.content]: i
       }
 
-      const group = {
-        // get the value of #=[] on forNode
-        hash: slice(context, $hash.path),
-        deps: []
-      }
-
-      group.nodes = forNodes.map(node => {
-        const cloneNode = node.cloneNode(true)
-        sweetify(node, cloneNode)
-        if (cloneNode.sweet) cloneNode.sweet.isProcessed = false
-
-        // pass the context to all child forNodes
-        traverse(cloneNode, (childNode) => {
-          if (childNode.sweet) {
-            childNode.sweet.context = context
-            childNode.sweet.addDeps = fn => group.deps.push(fn)
-          }
-        })
-
-        // then process
-        processNode.call(this, cloneNode)
-
-        this.delayedProcesses.push(() => {
-          forNode.before(cloneNode)
-        })
-
-        return cloneNode
+      this.delayedProcesses.push(() => {
+        forNode.before(loopComponentInstance)
       })
-
-      groups.push(group)
     })
   }
 
   renderLoop()
-  console.log(groups)
-
-  // when array changes, update
-
-  console.log({ path: $of.path })
+  render(name)
 
   const onArrayChange = () => {
     console.log('array changed')
