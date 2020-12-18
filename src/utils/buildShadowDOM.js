1d1bf17b1b5307819a669144a1c0dd11113963d5
Manan Tank <manantankm@gmail.com>
2020-12-18 12:39:54 +0530

    add comments in buildShadowDOM

--- src/utils/buildShadowDOM.js
+++ src/utils/buildShadowDOM.js
@@ -2,11 +2,20 @@ import sweetify from './node/sweetify.js'
 import processNode from './process/processNode.js'
 
 function buildShadowDOM (template) {
-  this.attachShadow({ mode: this.memo.mode })
+  // create clone of template
   const fragment = template.content.cloneNode(true)
+
+  // add sweet property on nodes
   sweetify(template.content, fragment)
+
+  // process nodes using sweet property
   processNode.call(this, fragment)
-  this.delayedProcesses.forEach(p => p());
+
+  // perform fragment modification which is part of processing
+  this.delayedProcesses.forEach(p => p())
+
+  // add fragment to shadow DOM
+  this.attachShadow({ mode: this.memo.mode });
   [...fragment.childNodes].forEach(node => this.shadowRoot.append(node))
 }
 
