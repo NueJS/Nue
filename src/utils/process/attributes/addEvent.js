a86730eed92438b3dab45187fd8030a25354a5ff
Manan Tank <manantankm@gmail.com>
2020-12-19 17:17:22 +0530

    use findFn in addEvent

--- src/utils/process/attributes/addEvent.js
+++ src/utils/process/attributes/addEvent.js
@@ -1,10 +1,13 @@
 import { supersweet } from '../../../index.js'
+import getFn from '../../closure.js'
 import { addConnects } from '../../node/connections.js'
 
 function addEvent (node, info) {
   const { name, placeholder } = info
   const action = supersweet.actions[name]
-  const handler = this.fn[placeholder.content]
+
+  const fnName = placeholder.content
+  const handler = getFn(this, fnName)
 
   // @TODO move this in dev condition
   if (!handler) throw new Error(`"ERROR in <${this.nodeName}>'s <${node.nodeName}> : "${placeholder.content}" function is not defined`)
