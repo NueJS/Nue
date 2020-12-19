6c4bbd1f0388bdd863bd6734e66317a845df77f6
Manan Tank <manantankm@gmail.com>
2020-12-19 17:18:58 +0530

    code refactor in reactify.js

--- src/utils/reactivity/reactify.js
+++ src/utils/reactivity/reactify.js
@@ -7,44 +7,43 @@ import deepEqual from '../deepEqual.js'
 const isObject = x => typeof x === 'object' && x !== null
 
 // create a reactive object which when mutated calls the on_change function
-function reactify (state, path = []) {
+function reactify (obj, path = []) {
   const closure$ = this.sweet && this.sweet.closure.$
-  if (!isObject(state)) return state
+  if (!isObject(obj)) return [obj]
 
-  const wrapper = Array.isArray(state) ? [] : {}
-  Object.keys(state).forEach(key => {
-    [wrapper[key]] = reactify.call(this, state[key], [...path, key])
+  const target = Array.isArray(obj) ? [] : {}
+
+  // debugger
+  Object.keys(obj).forEach(key => {
+    [target[key]] = reactify.call(this, obj[key], [...path, key])
   })
 
   const _this = this
-  const reactive = new Proxy(wrapper, {
+
+  const reactive = new Proxy(target, {
+
     set (target, prop, newValue) {
+      let success
       // if the prop is not available in target
       // try and find it in parent
       // if found in parent return it
-      // else set it in this state
-      if (!(prop in target) && !modes.noOverride) {
+      // else set it in this obj
+      if (!modes.noOverride && !(prop in target)) {
         // console.log('prop', prop, 'not in', target)
         if (closure$) {
-          const status = Reflect.set(closure$, prop, newValue)
-          if (status) {
-            return status
-          }
+          const success = Reflect.set(closure$, prop, newValue)
+          if (success) return success
         }
       }
 
       let value = newValue
       if (modes.noOverride) {
-        if (target.hasOwnProperty(prop)) {
-          console.log(prop, 'is in ', target)
-          return true
-        }
+        if (prop in target) return true
         if (typeof value === 'function') value = computedState.call(_this, value, prop)
       }
 
-      if (isObject(value)) value = reactify.call(_this, value, [...path, prop])
+      if (isObject(value)) [value] = reactify.call(_this, value, [...path, prop])
 
-      let success
       if (modes.reactive) {
         if (!(prop === 'length' && Array.isArray(target))) {
           if (!deepEqual(target[prop], value)) {
@@ -68,17 +67,17 @@ function reactify (state, path = []) {
         else accessed.paths.push([...path, prop])
       }
 
-      if (prop === '__target__') return wrapper
       if (prop === '__parent__') return closure$
       if (prop === '__host__') return _this
 
       if (prop in target) return Reflect.get(target, prop)
-      else return Reflect.get(closure$, prop)
+      if (closure$) return Reflect.get(closure$, prop)
+      return undefined
     }
 
   })
 
-  return [reactive, wrapper]
+  return [reactive, target]
 }
 
 export default reactify
