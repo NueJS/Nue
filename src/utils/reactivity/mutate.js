9e6b2d4a7248daed078d5bf12b191b3ee9456dde
Manan Tank <manantankm@gmail.com>
2020-12-20 21:54:55 +0530

    use targetProp in mutate

--- src/utils/reactivity/mutate.js
+++ src/utils/reactivity/mutate.js
@@ -1,13 +1,6 @@
-// mutate the object using path to find target,
-// value to to change the value on target
-// and trap to define the kind of change
-
-// example:
-// to do -> obj.x.y.z = 100, call mutate like this:
-// mutate(obj, ['x', 'y', 'z'], 100, 'set')
+import { targetProp } from '../state/slice.js'
 
 export const mutate = (obj, path, value, trap) => {
-  const target = path.slice(0, -1).reduce((acc, key) => acc[key], obj)
-  const prop = path[path.length - 1]
+  const [target, prop] = targetProp(obj, path)
   return Reflect[trap](target, prop, value)
 }
