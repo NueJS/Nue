ee40ef960a0bf6c3306efc0be78f8b679c326252
Manan Tank <manantankm@gmail.com>
2020-12-20 21:52:28 +0530

    new conditional rendering

--- src/utils/process/if/createGroups.js
+++ src/utils/process/if/createGroups.js
@@ -1,46 +1,98 @@
-function createGroups (conditionNode, deps, groups, ifNode) {
+import { disconnect } from '../../node/connections.js'
+import traverse from '../../node/traverse.js'
+import satisfies from './comparison.js'
+import { removeGroup } from './group.js'
+
+function createGroups (ifNode, conditionNode = ifNode, groupDeps = [], groups = []) {
   const { sweet } = conditionNode
+  let lastNode
+  let compareWith
+  const type = conditionNode.nodeName
+  const animate = conditionNode.hasAttribute('animate')
 
+  // group creation ---------------------------
   const group = {
-    type: conditionNode.nodeName,
-    nodes: [],
-    added: false,
-    prevAdded: undefined,
-    processed: false,
     conditionNode,
-    animate: conditionNode.getAttribute('animate')
-
-  }
-
-  if (sweet && sweet.attributes) {
-    const { placeholder } = sweet.attributes[0]
-    group.placeholder = placeholder
-    group.compareWith = sweet.attributes[0].name
-    group.anchorNode = document.createComment(` ${conditionNode.nodeName} ${group.compareWith}=[${group.placeholder.content}] ❌ `)
-    placeholder.deps.forEach(d => deps.push(d))
-  } else {
-    group.anchorNode = document.createComment(` ${conditionNode.nodeName} ❌ `)
+    nodes: [],
+    isRendered: false,
+    isProcessed: false,
+    disconnect: () => {
+      group.nodes.forEach(node => traverse(node, disconnect))
+    },
+    onRemove: (cb) => {
+      lastNode.addEventListener('animationend', cb, { once: true })
+    }
   }
 
   groups.push(group)
 
-  // after creating the groups, add anchorNodes for each group
+  if (type !== 'ELSE') {
+    const { placeholder } = sweet.attributes[0]
+
+    // add deps on conditionNode to groupDeps
+    placeholder.deps.forEach(d => groupDeps.push(d))
+
+    // @TODO - do not trust that 0th with be the condition
+    compareWith = sweet.attributes[0].name
+
+    group.anchorNode = document.createComment(` ${conditionNode.nodeName} ${compareWith}=${placeholder.text} ❌ `)
+
+    group.isSatisfied = () => {
+      const value = placeholder.getValue.call(this)
+      return satisfies(value, compareWith)
+    }
+  }
+
+  else {
+    group.anchorNode = document.createComment(` ${conditionNode.nodeName} ❌ `)
+    // else does not have any condition and thus is always satisfied
+    group.isSatisfied = () => true
+  }
+
+  // if group has animate, add animate attribute on all the nodes in the group
+  if (animate) {
+    group.animate = conditionNode.getAttribute('animate')
+    group.remove = () => {
+      group.nodes.forEach(node => node.setAttribute('exit', ''))
+      lastNode.addEventListener('animationend', () => removeGroup(group), { once: true })
+    }
+  }
+
+  else {
+    group.remove = () => removeGroup(group)
+  }
+
+  // after processed
+  // remove conditionNode, and all the nodes
+  // add comment anchorNode
   this.delayedProcesses.push(() => {
     ifNode.before(group.anchorNode)
+    group.nodes.forEach(n => n.remove())
   })
 
-  conditionNode.childNodes.forEach(node => {
-    if (node.nodeName === 'ELSIF' || node.nodeName === 'ELSE' || node.nodeName === 'IF') {
-      createGroups.call(this, node, deps, groups, ifNode)
-    } else {
-      group.nodes.push(node)
-      // pretend as if this is processed to avoid the traverse function from processing this node
-      if (node.sweet) node.sweet.isProcessed = true
-
-      // if group has animate, add animate attribute on all the nodes in the group
-      if (group.animate) node.setAttribute('animate', group.animate)
+  for (const node of conditionNode.childNodes) {
+    // if this node is text node
+    if (node.nodeType === Node.TEXT_NODE) {
+      throw new Error('text node is not direct child of <if> when animate attribute is added. wrap the textNode in span')
     }
-  })
+
+    // if is is condition node
+    else if (node.nodeName === 'ELSIF' || node.nodeName === 'ELSE' || node.nodeName === 'IF') {
+      createGroups.call(this, ifNode, node, groupDeps, groups)
+    }
+
+    else {
+      group.nodes.push(node)
+      // add class given in the group.animate to all the direct child nodes
+      if (animate) node.classList.add(group.animate)
+      // do not process node
+      if (node.sweet) node.sweet.isProcessed = true
+    }
+  }
+
+  lastNode = group.nodes[group.nodes.length - 1]
+
+  return { groups, groupDeps }
 }
 
 export default createGroups
--- src/utils/process/if/if.js
+++ src/utils/process/if/if.js
@@ -1,90 +1,58 @@
-// import slice from '../../state/slice.js'
-import { addGroup, removeGroup, processGroup } from './group.js'
-// import { FN, REACTIVE } from '../../constants.js'
+import { addGroup, processGroup } from './group.js'
 import createGroups from './createGroups.js'
-import satisfies from './comparison.js'
-import addDep from '../../state/addDep.js'
+import { addDeps } from '../../state/addDep.js'
 
 function process_if (ifNode) {
-  // nodes inside conditional nodes are divided into groups
-  const groups = []
+  // binding
+  const _createGroups = createGroups.bind(this)
+  const _processGroup = processGroup.bind(this)
+  const _addDeps = addDeps.bind(this)
 
-  // combinations of groupDeps of all the condition node's groupDeps
-  // when any of the conditions change, check for re-render
-  const groupDeps = []
-
-  // divide the nodes in groups and after delay, add the nodes in DOM
-  createGroups.call(this, ifNode, groupDeps, groups, ifNode)
+  const { groups, groupDeps } = _createGroups(ifNode)
 
   // group that is currently rendered
-  let activeGroup
+  let prevRenderedGroup
 
-  const handleConditionChange = () => {
-    // if a group's condition is truthy,
-    // all other groups after it should not be rendered even if they are true
-    let true_found = false
+  const groupDepChanged = () => {
+    // if a group's condition is foundSatisfied, this becomes true
+    let foundSatisfied = false
 
-    // for each group check if it should be rendered or not based on new condition value
     groups.forEach((group) => {
-      const { placeholder } = group
-      let condition_value = true // default value for else group
+      // if this group should be rendered
+      if (!foundSatisfied && group.isSatisfied()) {
+        foundSatisfied = true
 
-      // compute condition value
-      // if true is found already no need to check the value, assume its true
-      if (!true_found && placeholder) {
-        condition_value = placeholder.getValue.call(this)
-      }
+        // if this group is never processed before, process it first
+        if (!group.isProcessed) _processGroup(group)
 
-      // show group
-      // if previous active group is animating its exit, wait for it to complete
-      // and then add the group
-      // add enter attribute on node
-      if (!true_found && satisfies(condition_value, group.compareWith)) { // ADD
-        true_found = true
-
-        // if group should be rendered, but is already rendered - no need to render it
-        if (!group.added) {
-          if (!group.processed) processGroup.call(this, group)
-
-          if (activeGroup && activeGroup.animate && group !== activeGroup) {
-            const lastIndex = activeGroup.nodes.length - 1
-            activeGroup.nodes[lastIndex].addEventListener('animationend', () => addGroup(group), { once: true })
+        // if this group is not already rendered
+        if (!group.isRendered) {
+          // if group should wait for other group's animation end
+          if (prevRenderedGroup && prevRenderedGroup.animate && group !== prevRenderedGroup) {
+            prevRenderedGroup.onRemove(() => addGroup(group))
           } else {
+            console.log('render : ', group)
             addGroup(group, groups.anchorNode)
           }
 
-          activeGroup = group
+          prevRenderedGroup = group
         }
       }
 
-      // remove the group
-      // if group should have animated exit
-      // add exit attribute on all the nodes to start the animation
-      // once the last animation completes, remove all the nodes
-      else if (group.added) {
-        if (group.animate) {
-          group.nodes.forEach(node => node.setAttribute('exit', ''))
-          const lastIndex = group.nodes.length - 1
-          const lastNode = group.nodes[lastIndex]
-          lastNode.addEventListener('animationend', () => removeGroup(group), { once: true })
-        } else {
-          removeGroup(group)
-        }
+      // if the group should be removed
+      else if (group.isRendered) {
+        group.disconnect()
+        group.remove()
       }
     })
   }
 
+  _addDeps(groupDeps, groupDepChanged, 'stateReady')
+
   this.delayedProcesses.push(() => {
-    groups.forEach(g => {
-      g.conditionNode.remove()
-      g.nodes.forEach(n => n.remove())
-    })
-
-    handleConditionChange()
-  })
-
-  groupDeps.forEach(dep => {
-    addDep.call(this, dep, handleConditionChange, 'stateReady')
+    ifNode.after(document.createComment(' / IF '))
+    ifNode.remove()
+    groupDepChanged()
   })
 }
 
