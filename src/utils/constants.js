636cf8bd1abe8fba741b6f1337f9c858849281b7
Manan Tank <manantankm@gmail.com>
2020-12-21 14:51:53 +0530

    support for conditionally adding attributes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,6 +4,7 @@ export const NORMAL = Symbol('NORMAL')
 export const EVENT = Symbol('EVENT')
 export const STATE = Symbol('STATE')
 export const BIND = Symbol('BIND')
+export const CONDITIONAL = Symbol('CONDITIONAL')
 
 // placeholder or split type
 export const REACTIVE = Symbol('REACTIVE')
--- src/utils/process/attributes/addAttribute.js
+++ src/utils/process/attributes/addAttribute.js
@@ -1,13 +1,25 @@
+import { CONDITIONAL } from '../../constants.js'
 import { wire } from '../../node/connections.js'
 
 // example: data-count=[XXX]
 function addAttribute (node, attribute) {
-  const { placeholder, name } = attribute
+  const { placeholder, name, type } = attribute
   const { deps, getValue } = placeholder
 
+  let update
+  if (type === CONDITIONAL) {
+    update = () => {
+      const value = getValue.call(this)
+      if (value) node.setAttribute(name, value)
+      else node.removeAttribute(name)
+    }
+  }
+
   // update attribute value when called with latest from state
-  const update = () => {
-    node.setAttribute(name, getValue.call(this))
+  else {
+    update = () => {
+      node.setAttribute(name, getValue.call(this))
+    }
   }
 
   wire.call(this, node, deps, update)
