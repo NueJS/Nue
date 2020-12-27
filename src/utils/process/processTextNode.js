954c0e7ad9a721958d83cc1ff80991f61210174d
Manan Tank <manantankm@gmail.com>
2020-12-27 17:38:33 +0530

    rename textNode to processTextNode

--- src/utils/process/processTextNode.js
+++ src/utils/process/processTextNode.js
@@ -2,17 +2,17 @@ import { wire } from '../node/connections.js'
 import isInvalidPlaceholder from '../string/placeholder/isInvalidPlaceholder.js'
 
 function processTextNode (comp, node) {
-  const { getValue, deps, text } = node.sweet.placeholder
+  const { getValue, deps } = node.sweet.placeholder
   const notPlaceholder = isInvalidPlaceholder(comp, getValue)
 
-  if (!notPlaceholder) {
+  if (notPlaceholder) {
+    node.sweet = undefined
+  }
+  else {
     const update = () => {
       node.textContent = getValue(comp, node)
     }
     wire(comp, node, deps, update)
-  } else {
-    node.textContent = text
-    node.sweet = undefined
   }
 }
 
