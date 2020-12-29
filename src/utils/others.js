ae226b1acfba92223a5ce546c7b53e911a207dc4
Manan Tank <manantankm@gmail.com>
2020-12-29 19:12:45 +0530

    add swap and uid utils

--- src/utils/others.js
+++ src/utils/others.js
@@ -8,3 +8,9 @@ export function reverseForEach (arr, cb) {
     cb(arr[i], i)
   }
 }
+
+export const swap = (arr, i, j) => {
+  [arr[i], arr[j]] = [arr[j], arr[i]]
+}
+
+export const uid = () => Math.random().toString(36).replace(/[^a-z]+/g, '')
