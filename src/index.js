92091e489f5dd754d4e46b841e0a519fda80f7c1
Manan Tank <manantankm@gmail.com>
2021-01-04 00:04:32 +0530

    use errors object

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import defineComponent from './defineComponent.js'
 import globalInfo from './globalInfo.js'
 import DEV from './utils/dev/DEV.js'
 import err from './utils/dev/error.js'
+import errors from './utils/dev/errors.js'
 export { default as settings } from './settings.js'
 
 export const defineActions = (obj) => {
@@ -16,11 +17,7 @@ export const render = (name) => {
   const comp = globalInfo.components[name]
 
   if (DEV) {
-    if (!comp) {
-      err({
-        message: `tried to render component <${name}>, but not defined earlier.`
-      })
-    }
+    if (!comp) errors.RENDER_CALLED_BEFORE_DEFINE(name)
   }
 
   globalInfo.renderedComps[name] = true
