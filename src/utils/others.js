75d9c86d4f6b78df65b4d26136764728137cd76e
Manan Tank <manantankm@gmail.com>
2021-01-04 00:05:22 +0530

    add insert in others.js

--- src/utils/others.js
+++ src/utils/others.js
@@ -1,8 +1,3 @@
-/**
- *
- * @param {Array} arr
- * @param {Function} cb
- */
 export function reverseForEach (arr, cb) {
   for (let i = arr.length - 1; i >= 0; i--) {
     cb(arr[i], i)
@@ -14,3 +9,14 @@ export const swap = (arr, i, j) => {
 }
 
 export const uid = () => Math.random().toString(36).replace(/[^a-z]+/g, '')
+
+export const insert = (arr, i, value) => {
+  arr.splice(i, 0, value)
+}
+
+// export const promisifyWorker = worker => (...args) => new Promise(resolve => {
+//   worker.postMessage(args)
+//   worker.onmessage = (e) => {
+//     resolve(e.data)
+//   }
+// })
