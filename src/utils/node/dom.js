4c9e45ad14d09468371b69a9cf5355a818959353
Manan Tank <manantankm@gmail.com>
2021-01-04 00:07:58 +0530

    add utils in dom.js

--- src/utils/node/dom.js
+++ src/utils/node/dom.js
@@ -1,2 +1,26 @@
 export const isConditionNode = node =>
   ['IF', 'ELSE-IF', 'ELSE'].includes(node.nodeName)
+
+export const getOffset = (node) => {
+  return {
+    left: node.offsetLeft,
+    top: node.offsetTop
+  }
+}
+
+export const attr = (node, name) => node.getAttribute(name)
+
+export const animate = (node, name) => {
+  node.style.animation = name
+}
+
+export const waitForEvent = async (node, eventName) =>
+  new Promise(resolve =>
+    node.addEventListener(eventName, resolve, { once: true })
+  )
+
+export const saveOffsets = (comps) => {
+  for (const c of comps) {
+    c.prev = getOffset(c)
+  }
+}
