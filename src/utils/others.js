a435c11636ca01969a8b60885b3085b8e6bcf46d
Manan Tank <manantankm@gmail.com>
2021-01-04 12:24:38 +0530

    stable uid

--- src/utils/others.js
+++ src/utils/others.js
@@ -8,7 +8,10 @@ export const swap = (arr, i, j) => {
   [arr[i], arr[j]] = [arr[j], arr[i]]
 }
 
-export const uid = () => Math.random().toString(36).replace(/[^a-z]+/g, '')
+// export const uid = () => Math.random().toString(36).replace(/[^a-z]+/g, '')
+
+let i = 0
+export const uid = () => '' + i++
 
 export const insert = (arr, i, value) => {
   arr.splice(i, 0, value)
