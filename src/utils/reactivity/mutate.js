a17f2d0b406dc6358b16a4dd0fb9b03b6b27d372
Manan Tank <manantankm@gmail.com>
2021-01-06 11:36:01 +0530

    make the trap optional in mutate()

--- src/utils/reactivity/mutate.js
+++ src/utils/reactivity/mutate.js
@@ -1,6 +1,6 @@
 import { targetProp } from '../state/slice.js'
 
-export const mutate = (obj, path, value, trap) => {
+export const mutate = (obj, path, value, trap = 'set') => {
   const [target, prop] = targetProp(obj, path)
   return Reflect[trap](target, prop, value)
 }
