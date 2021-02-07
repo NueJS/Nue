606e439380ac67f288723d8f404efa6d57a1b889
Manan Tank <manantankm@gmail.com>
2021-02-07 10:45:48 +0530

    remove correction API from processTextNode

--- src/utils/process/processTextNode.js
+++ src/utils/process/processTextNode.js
@@ -1,19 +1,11 @@
 import wire from '../connection/wire.js'
-import isInvalidPlaceholder from '../string/placeholder/isInvalidPlaceholder.js'
 
 function processTextNode (comp, node) {
   const { getValue, deps } = node.sweet.placeholder
-  const notPlaceholder = isInvalidPlaceholder(comp, getValue)
-
-  if (notPlaceholder) {
-    node.sweet = undefined
+  const update = () => {
+    node.textContent = getValue(comp, node)
   }
-  else {
-    const update = () => {
-      node.textContent = getValue(comp, node)
-    }
-    wire(comp, node, deps, update)
-  }
+  wire(comp, node, deps, update)
 }
 
 export default processTextNode
