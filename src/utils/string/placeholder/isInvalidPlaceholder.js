930f30fcf18dabf04b47353d2c5937485321685b
Manan Tank <manantankm@gmail.com>
2021-02-07 10:48:47 +0530

    comment out isInvalidPlaceholder as it's not used

--- src/utils/string/placeholder/isInvalidPlaceholder.js
+++ src/utils/string/placeholder/isInvalidPlaceholder.js
@@ -1,8 +1,8 @@
-// in not a valid placeholder, turn it into string
-const isInvalidPlaceholder = (comp, getValue) => {
-  let value
-  try { value = getValue(comp) } catch { /**/ }
-  return value === undefined
-}
+// // in not a valid placeholder, turn it into string
+// const isInvalidPlaceholder = (comp, getValue) => {
+//   let value
+//   try { value = getValue(comp) } catch { /**/ }
+//   return value === undefined
+// }
 
-export default isInvalidPlaceholder
+// export default isInvalidPlaceholder
