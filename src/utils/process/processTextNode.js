99d2a9e62e26adebbba70247a5c8d92ee8ed81f0
Manan Tank <manantankm@gmail.com>
2021-02-11 01:09:37 +0530

    no need to call getValue with node

--- src/utils/process/processTextNode.js
+++ src/utils/process/processTextNode.js
@@ -3,7 +3,7 @@ import wire from '../connection/wire.js'
 function processTextNode (comp, node) {
   const { getValue, deps } = node.sweet.placeholder
   const update = () => {
-    node.textContent = getValue(comp, node)
+    node.textContent = getValue(comp)
   }
   wire(comp, node, deps, update)
 }
