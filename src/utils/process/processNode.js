b1b48cb5cf68ac87bae5aeea51f0ec7632f35170
Manan Tank <manantankm@gmail.com>
2021-02-14 03:31:26 +0530

    fix: slot- do not process childNodes of component

--- src/utils/process/processNode.js
+++ src/utils/process/processNode.js
@@ -6,12 +6,18 @@ import processFor from './for/processFor.js'
 function processNode (comp, node) {
   const { parsed, nodeType, nodeName } = node
   if (parsed) {
-    if (parsed.isProcessed) return
+    // when would we be trying to process the already processed node ?
+    // is this even necessary ?
+    if (parsed.isProcessed) {
+      console.log('already processed : ', node)
+      return
+    }
     parsed.isProcessed = true
 
     // save the comp as closure of component
     if (parsed.isComp) {
       node.parsed.closure = comp
+      // if the component is being used in for loop
       if (parsed.for) processFor(comp, node)
     }
     else if (nodeType === Node.TEXT_NODE) processTextNode(comp, node)
@@ -19,9 +25,8 @@ function processNode (comp, node) {
     if (node.hasAttribute) processAttributes(comp, node)
   }
 
-  if (nodeName === 'FOR') return
-
-  if (node.hasChildNodes()) {
+  // if it a component, do not process it's child nodes
+  if (!(parsed && parsed.isComp) && node.hasChildNodes()) {
     node.childNodes.forEach(n => processNode(comp, n))
   }
 }
