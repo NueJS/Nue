2f67e67d925f0097f118f9a9a7605ec51da5a6ab
Manan Tank <manantankm@gmail.com>
2021-02-13 18:36:31 +0530

    fix: state attributes

--- src/utils/process/attributes/addState.js
+++ src/utils/process/attributes/addState.js
@@ -1,11 +1,20 @@
-// :name=[path]
+import { mutate } from '../../reactivity/mutate'
+import { addDeps } from '../../state/addDep'
+
+// name=[path]
 function addState (comp, node, attribute) {
   const { name, placeholder } = attribute
-  const { getValue } = placeholder
-
+  const { getValue, deps } = placeholder
   // pass the stateProps to child component to initialize state
   if (!node.parsed.stateProps) node.parsed.stateProps = {}
   node.parsed.stateProps[name] = getValue(comp)
+
+  // when the parent state changes, update the state of child component
+  const cb = () => {
+    mutate(node.nue.$, [name], getValue(comp))
+  }
+
+  addDeps(comp, deps, cb, 'computed')
 }
 
 export default addState
