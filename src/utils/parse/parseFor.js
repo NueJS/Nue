056bac56b67711669d48561df536eb74b948ee65
Manan Tank <manantankm@gmail.com>
2021-02-13 18:39:16 +0530

    refactor: key attribute should not be given placeholder value

--- src/utils/parse/parseFor.js
+++ src/utils/parse/parseFor.js
@@ -3,7 +3,7 @@ import { attr } from '../node/dom'
 import processPlaceholder from '../string/placeholder/processPlaceholder'
 import { checkFor } from './checkParsed'
 
-const parseFor = (node, forAttribute) => {
+const parseFor = (comp, node, forAttribute) => {
   // replace ' in ', '(' ')' ',' with space, split with space, and remove empty strings
   const arr = forAttribute.replace(/\(|\)|,|(\sin\s)/g, ' ').split(/\s+/).filter(t => t)
   const atUsed = arr.length === 3
@@ -12,7 +12,7 @@ const parseFor = (node, forAttribute) => {
     map: processPlaceholder(atUsed ? arr[2] : arr[1], true),
     as: arr[0],
     at: atUsed && arr[1],
-    key: processPlaceholder(attr(node, 'key')),
+    key: processPlaceholder(attr(node, 'key'), true),
     enter: attr(node, 'enter'),
     reorder: attr(node, 'reorder'),
     exit: attr(node, 'exit')
@@ -22,7 +22,7 @@ const parseFor = (node, forAttribute) => {
     node.removeAttribute(name)
   })
 
-  if (DEV) checkFor(node, arr)
+  if (DEV) checkFor(comp, node, arr)
 }
 
 export default parseFor
