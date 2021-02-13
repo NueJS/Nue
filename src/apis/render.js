877770b29990e5016093f853e429702b359397df
Manan Tank <manantankm@gmail.com>
2021-02-14 03:16:30 +0530

    remove unused errors

--- src/apis/render.js
+++ src/apis/render.js
@@ -1,6 +1,4 @@
 import globalInfo from '../utils/globalInfo'
-import DEV from '../utils/dev/DEV'
-import errors from '../utils/dev/errors'
 import defineComponent from '../utils/defineComponent'
 import dashify from '../utils/string/dashify'
 import showError from '../utils/dev/error-overlay/showError.js'
@@ -14,18 +12,13 @@ const render = (component, settings) => {
   if (settings) {
     globalInfo.defaultStyle = settings.defaultStyle
   }
+
   const name = component.name
 
   // find the <CompName> in html and replace it with <compname->
   const el = document.querySelector(name)
   const root = document.createElement(dashify(name))
   el.replaceWith(root)
-
-  if (DEV) {
-    if (globalInfo.components[name]) {
-      errors.COMPONENT_ALREADY_RENDERED(name)
-    }
-  }
 
   defineComponent(component.name, component)
 }
--- src/utils/dev/errors.js
+++ src/utils/dev/errors.js
@@ -8,20 +8,6 @@ export default {
     }
   },
 
-  COMPONENT_ALREADY_RENDERED (name) {
-    throw {
-      message: `<${name}> is rendered already, no need to call render("${name}") again`,
-      fix: `remove the extra call to render("${name}")`
-    }
-  },
-
-  FN_CALLED_MORE_THAN_ONCE (fnName) {
-    throw {
-      message: `${fnName}() is called more than once, it should only be called once for the lifetime of application`,
-      fix: `remove the extra calls to ${fnName}()`
-    }
-  },
-
   KEYS_ARE_NOT_UNIQUE (keys, component) {
     const nonUniqueKeys = keys.filter((key, i) => {
       return keys.indexOf(key, i) !== keys.lastIndexOf(key)
