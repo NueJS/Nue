f9846c6201e7d2a48ffa2ec39df3e7fe8cf3a6ec
Manan Tank <manantankm@gmail.com>
2021-02-14 03:17:18 +0530

    rename copySweet to copyParsed

--- src/utils/node/copyParsed.js
+++ src/utils/node/copyParsed.js
@@ -1,15 +1,15 @@
 // copy .parsed properties from node's tree to cloneNode's tree
 // cloneNode is clone of node but it does not have custom .parsed properties added in node's tree
 // sweetify is used to add those properties back in the cloneNode
-const copySweet = (node, cloneNode) => {
+const copyParsed = (node, cloneNode) => {
   // do not give the ref directly, make a shallow clone
-  // @TODO, use deepclone ??
-  if (node.parsed) cloneNode.parsed = { ...node.parsed }
+  // @TODO, use deep clone ??
+  if (node.parsed) cloneNode.parsed = { ...node.parsed, isProcessed: false }
   if (node.hasChildNodes()) {
     node.childNodes.forEach((n, i) => {
-      copySweet(n, cloneNode.childNodes[i])
+      copyParsed(n, cloneNode.childNodes[i])
     })
   }
 }
 
-export default copySweet
+export default copyParsed
