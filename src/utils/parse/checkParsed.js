b131dc8acdc9620e209c7248d5a8160977a5f3ee
Manan Tank <manantankm@gmail.com>
2021-02-13 18:38:00 +0530

    feature: show error when the exit animation is specified but not defined

--- src/utils/parse/checkParsed.js
+++ src/utils/parse/checkParsed.js
@@ -1,6 +1,6 @@
 import err from '../dev/error'
 
-export const checkFor = (node, arr) => {
+export const checkFor = (comp, node, arr) => {
   if (arr.length < 2) {
     err({
       message: `invalid for attribute value on ${node.nodeName}`,
@@ -9,7 +9,26 @@ export const checkFor = (node, arr) => {
     })
   }
 
-  if (!node.parsed.for.key) {
+  const { key, exit } = node.parsed.for
+
+  if (exit) {
+    comp.deferred.push(() => {
+      const [animationName] = exit.split(' ')
+      const style = comp.memo.template.content.querySelector('style:not([default-styles])')
+      // style.textContent
+      const styleUsesAnimation = style.textContent.includes(animationName)
+      if (!styleUsesAnimation) {
+        err({
+          message: `exit animation: "${animationName}" used on <${node.parsed.name}> but not defined in CSS. \nThis will result in component never being removed, as nue.js keeps waiting for the animation to end which does not exist`,
+          fix: `To fix this: define animation "${animationName}" in CSS using @keyframes`,
+          comp,
+          code: 'MISSING_EXIT_ANIMATION_IN_CSS'
+        })
+      }
+    })
+  }
+
+  if (!key) {
     err({
       message: `missing attribute "key" on ${node.nodeName}.`,
       code: 2,
