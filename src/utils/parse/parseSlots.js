4656d70a748ae4a1633a32e85fa2e0a1908413f4
Manan Tank <manantankm@gmail.com>
2021-02-13 19:49:02 +0530

    add comments in parseSlots

--- src/utils/parse/parseSlots.js
+++ src/utils/parse/parseSlots.js
@@ -1,23 +1,31 @@
 function parseSlots (comp) {
-  // if we need nodes in slot
+  // if the component had childNodes
   if (comp.parsed && comp.parsed.childNodes) {
+    // for each childNode
     comp.parsed.childNodes.forEach(node => {
+      // assume that this node does not have slot attribute, so it goes where the unnamed slot is
       let slotSelector = 'slot:not([name])'
-      if (node.nodeType !== Node.TEXT_NODE) {
-        // slotSelector = 'slot'
-        const slotName = node.getAttribute('slot')
-        node.removeAttribute('slot')
 
+      // if the node is not text node
+      if (node.nodeType !== Node.TEXT_NODE) {
+        // get the slot name from slot attribute
+        const slotName = node.getAttribute('slot')
+
+        // if the slot name is present
         if (slotName) {
+          node.removeAttribute('slot')
+          // this node will be pushed in the where the named slot is
           slotSelector = `slot[name=${slotName}]`
         }
       }
 
+      // find the target slot in the template of component
       const slot = comp.memo.template.content.querySelector(slotSelector)
+      // if the target slot is found, add the node before the slot
       if (slot) slot.before(node)
     })
 
-    // remove all slots
+    // after all the the childNodes are added in template, remove the slot nodes from template
     comp.memo.template.content.querySelectorAll('slot').forEach(s => s.remove())
   }
 }
