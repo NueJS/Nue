95f28ee14589a8151cb3097b81ae7136444350a0
Manan Tank <manantankm@gmail.com>
2021-02-13 12:53:05 +0530

    clear deferred array after running

--- src/utils/parse/parseTemplate.js
+++ src/utils/parse/parseTemplate.js
@@ -2,10 +2,10 @@ import parseNode from './parseNode'
 
 function parseTemplate (comp) {
   const uselessNodes = []
-  comp.deferred = []
   comp.memo.template.content.childNodes.forEach(n => parseNode(comp, n, uselessNodes))
   // run deferred methods
   comp.deferred.forEach(m => m())
+  comp.deferred = []
   // remove redundant nodes
   uselessNodes.forEach(n => n.remove())
 }
