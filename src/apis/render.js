781da74726594cd215360e0a53e79d9dc897139c
Manan Tank <manantankm@gmail.com>
2021-02-13 19:10:42 +0530

    add window.onerror

--- src/apis/render.js
+++ src/apis/render.js
@@ -3,6 +3,13 @@ import DEV from '../utils/dev/DEV'
 import errors from '../utils/dev/errors'
 import defineComponent from '../utils/defineComponent'
 import dashify from '../utils/string/dashify'
+import showError from '../utils/dev/error-overlay/showError.js'
+
+window.onerror = function (message, filename, lineno, colno, error) {
+  showError({
+    message: error.stack
+  })
+}
 
 // define the custom element of given name
 const render = (component, settings) => {
@@ -22,7 +29,11 @@ const render = (component, settings) => {
     }
   }
 
-  defineComponent(component.name, component)
+  try {
+    defineComponent(component.name, component)
+  } catch (e) {
+    console.log('caught error : ', e)
+  }
 }
 
 export default render
