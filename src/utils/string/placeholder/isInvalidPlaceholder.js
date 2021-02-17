f661c7f836f2d008278da4377a74fd1bd67af9a7
Manan Tank <manantankm@gmail.com>
2021-02-17 13:51:41 +0530

    remove Fn Placeholder API

--- src/utils/string/placeholder/isInvalidPlaceholder.js
+++
@@ -1,8 +0,0 @@
-// // in not a valid placeholder, turn it into string
-// const isInvalidPlaceholder = (comp, getValue) => {
-//   let value
-//   try { value = getValue(comp) } catch { /**/ }
-//   return value === undefined
-// }
-
-// export default isInvalidPlaceholder
--- src/utils/string/placeholder/processPlaceholder.js
+++ src/utils/string/placeholder/processPlaceholder.js
@@ -1,21 +1,14 @@
 import { unBracket } from '../bracket.js'
-import processFnPlaceholder from './processFnPlaceholder.js'
 import processReactivePlaceholder from './processReactivePlaceholder.js'
 
 // if functional placeholder's function name is not valid, make it not a placeholder
 function processPlaceholder (text, noBrackets = false) {
   // if the text has bracket, remove it
   const bracketsRemoved = noBrackets ? text : unBracket(text)
-
   // remove all spaces
   const content = bracketsRemoved.replace(/ /g, '')
 
-  // check for parens in content
-  const isFnPlaceholder = content.includes('(')
-
-  return isFnPlaceholder
-    ? processFnPlaceholder(content)
-    : processReactivePlaceholder(content)
+  return processReactivePlaceholder(content)
 }
 
 export default processPlaceholder
