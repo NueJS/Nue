f4a89f664699ec2504136147e0bc3cb287b4f67c
Manan Tank <manantankm@gmail.com>
2021-02-17 10:57:49 +0530

    showError in DEV only

--- src/apis/render.js
+++ src/apis/render.js
@@ -2,11 +2,14 @@ import globalInfo from '../utils/globalInfo'
 import defineComponent from '../utils/defineComponent'
 import dashify from '../utils/string/dashify'
 import showError from '../utils/dev/error-overlay/showError.js'
+import DEV from '../utils/dev/DEV'
 
 // define the custom element of given name
 const render = (component, settings) => {
-  window.onerror = function (message, filename, lineno, colno, error) {
-    showError(error)
+  if (DEV) {
+    window.onerror = function (message, filename, lineno, colno, error) {
+      showError(error)
+    }
   }
 
   if (settings) {
