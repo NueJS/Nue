c61d9498b695fb2c14e6c0cedffdf6f41026c07a
Manan Tank <manantankm@gmail.com>
2021-02-20 23:58:26 +0530

    remove unused: uid()

--- src/utils/others.js
+++ src/utils/others.js
@@ -2,11 +2,6 @@ export const swap = (arr, i, j) => {
   [arr[i], arr[j]] = [arr[j], arr[i]]
 }
 
-export const uid = /*#__PURE__ */ (() => {
-  let i = 0
-  return () => i++
-})()
-
 export const insert = (arr, i, value) => {
   arr.splice(i, 0, value)
 }
