45f458e33e165c9d14b9c1c8f653e8243317040f
Manan Tank <manantankm@gmail.com>
2021-02-20 22:56:11 +0530

    refactor: addAttribute

--- src/utils/process/attributes/addAttribute.js
+++ src/utils/process/attributes/addAttribute.js
@@ -1,23 +1,19 @@
 import { CONDITIONAL } from '../../constants.js'
 import wire from '../../connection/wire'
 
-// example: data-count=[XXX]
+// example: data-count=[XXX],  disabled:if=[XXx]
 function addAttribute (comp, node, attribute) {
   const { placeholder, name, type } = attribute
   const { deps, getValue } = placeholder
 
   let update
+
   if (type === CONDITIONAL) {
-    update = () => {
-      getValue(comp.$) ? node.setAttribute(name, '') : node.removeAttribute(name)
-    }
+    update = () => getValue(comp.$) ? node.setAttribute(name, '') : node.removeAttribute(name)
   }
 
-  // update attribute value when called with latest from state
   else {
-    update = () => {
-      node.setAttribute(name, getValue(comp.$))
-    }
+    update = () => node.setAttribute(name, getValue(comp.$))
   }
 
   wire(comp, node, deps, update)
