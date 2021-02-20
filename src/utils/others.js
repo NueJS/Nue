0f4bd1e246f4cab296c58c9a54bb8c11384dafa2
Manan Tank <manantankm@gmail.com>
2021-02-20 23:57:49 +0530

    remove unused: reverseForEach()

--- src/utils/others.js
+++ src/utils/others.js
@@ -1,9 +1,3 @@
-export function reverseForEach (arr, cb) {
-  for (let i = arr.length - 1; i >= 0; i--) {
-    cb(arr[i], i)
-  }
-}
-
 export const swap = (arr, i, j) => {
   [arr[i], arr[j]] = [arr[j], arr[i]]
 }
