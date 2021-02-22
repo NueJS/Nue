f2370093972286712207e3c03a4541af77864e2d
Manan Tank <manantankm@gmail.com>
2021-02-22 14:49:30 +0530

    refactor: addDep.js

--- src/utils/state/addDep.js
+++ src/utils/state/addDep.js
@@ -4,31 +4,33 @@ import DEV from '../dev/DEV.js'
 import errors from '../dev/errors.js'
 
 // add Dep for given path on its origin
-const addDep = (baseComp, path, cb, type) => {
-  const nue = origin(baseComp, path)
+const addDep = (baseNue, path, cb, type) => {
+  // get the nue where the state referred by path is coming from
+  const nue = origin(baseNue, path)
 
-  if (DEV && !nue) throw errors.STATE_NOT_FOUND(baseComp.name, path.join('.'))
+  // throw if no origin is found
+  if (DEV && !nue) throw errors.STATE_NOT_FOUND(baseNue.name, path.join('.'))
 
+  // get the higher order cb that will only call the cb once every batch
   const qcb = cbQueuer(nue, cb, type)
+
+  // start from the root of deps object
   let target = nue.deps
+
   const lastIndex = path.length - 1
 
+  // add qcb in dep table at appropriate location
   path.forEach((c, i) => {
     if (!target[c]) target[c] = { $: new Map() }
     target = target[c]
     if (i === lastIndex) target.$.set(qcb, true)
   })
 
-  // return cleanup to stop DOM updates when node is removed
-  if (type === 'dom') {
-    const removeDep = () => target.$.delete(qcb)
-    return removeDep
-  }
+  // return removeDep function that can remove the qcb from the deps table
+  return () => target.$.delete(qcb)
 }
 
-export const addDeps = (nue, deps, cb, type) => {
-  const removeDeps = deps.map(dep => addDep(nue, dep, cb, type))
-  return removeDeps
-}
+// returns an array of removeDep functions
+export const addDeps = (nue, deps, cb, type) => deps.map(dep => addDep(nue, dep, cb, type))
 
 export default addDep
