e1cc91d0a6fea79b1c71600198a6fd08ff39ea17
Manan Tank <manantankm@gmail.com>
2021-02-24 19:25:17 +0530

    update: optimize computedState

--- src/utils/state/computedState.js
+++ src/utils/state/computedState.js
@@ -6,16 +6,16 @@ import detectStateUsage from './detectStateUsage.js'
 // call that function, detect the state keys it depends on, get the initial value
 // update its value whenever its deps changes
 
-const computedState = (nue, fn, k) => {
+const computedState = (nue, fn, prop) => {
   const [initValue, paths] = detectStateUsage(fn)
 
   const compute = () => {
     const value = fn()
-    mutate(nue.$, [k], value)
+    mutate(nue.$, [prop], value)
   }
 
-  // when root of path changes value, recompute
-  const deps = paths.map(path => [path[0]])
+  const deps = paths.map(path => path.length === 1 ? path : path.slice(0, -1))
+
   addDeps(nue, deps, compute, 'computed')
   return initValue
 }
