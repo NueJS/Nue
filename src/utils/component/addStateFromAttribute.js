67e96309aa65de8f9d97e4641edf10885752e0ff
Manan Tank <manantankm@gmail.com>
2021-02-24 19:33:01 +0530

    rename: parentComp to parentNue in addStateFromAttribute

--- src/utils/component/addStateFromAttribute.js
+++ src/utils/component/addStateFromAttribute.js
@@ -1,22 +1,22 @@
 import subscribe from '../state/subscribe'
 import { hasSlice } from '../state/slice'
 
-function addStateFromAttribute (parentComp, nue, attribute) {
+function addStateFromAttribute (parentNue, nue, attribute) {
   const { name, placeholder } = attribute
   const { getValue, deps } = placeholder
 
   const cb = () => {
-    nue.$[name] = getValue(parentComp, nue.loopClosure)
+    nue.$[name] = getValue(parentNue, nue.loopClosure)
   }
 
-  nue.initState[name] = getValue(parentComp, nue.loopClosure)
+  nue.initState[name] = getValue(parentNue, nue.loopClosure)
 
   // if the attribute value depends on some part of state
   // when that part of state is changed update the attribute value
   deps.forEach(dep => {
-    if (hasSlice(parentComp.$, dep)) {
-      // cb is creating state using the parentComp and closure, so it should be done in computed queue
-      subscribe(parentComp, dep, cb, 'computed')
+    if (hasSlice(parentNue.$, dep)) {
+      // cb is creating state using the parentNue and closure, so it should be done in computed queue
+      subscribe(parentNue, dep, cb, 'computed')
     }
   })
   // return removeDeps
