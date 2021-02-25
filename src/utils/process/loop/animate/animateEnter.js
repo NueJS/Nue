2548862c1c947c2b401c256f772dd78840e2f2d0
Manan Tank <manantankm@gmail.com>
2021-02-25 19:05:51 +0530

    refactor: animateEnter and animateRemove - use animate()

--- src/utils/process/loop/animate/animateEnter.js
+++ src/utils/process/loop/animate/animateEnter.js
@@ -1,12 +1,12 @@
+import { animate } from '../../../node/dom'
+
 const animateEnter = ({ createdComps, enter }) => {
   if (!enter) return
 
   createdComps.forEach(comp => {
     comp.style.visibility = null
-    comp.style.animation = enter
-    comp.addEventListener('animationend', () => {
-      comp.style.animation = null
-    }, { once: true })
+    // run enter animation for each created component
+    animate(comp, enter, true)
   })
 
   // clear array
--- src/utils/process/loop/animate/animateRemove.js
+++ src/utils/process/loop/animate/animateRemove.js
@@ -1,18 +1,22 @@
+import { animate } from '../../../node/dom'
 
 const animateRemove = (blob) => {
   const { exit, removedComps } = blob
 
   return new Promise(resolve => {
+    // go to next animation
     const next = () => resolve(blob)
+
+    // if no exit animation or no components, skip this
     if (!exit || !removedComps.length) next()
     else {
-      removedComps.forEach((comp, i) => {
-        comp.style.animation = exit
-        comp.addEventListener('animationend', () => {
-          comp.style.animation = null
+      removedComps.forEach((comp) => {
+        // run exit animation for each removed components
+        animate(comp, exit, true, () => {
+          // @todo use animatedRemove instead ?
           comp.remove()
           next()
-        }, { once: true })
+        })
       })
 
       removedComps.length = 0
