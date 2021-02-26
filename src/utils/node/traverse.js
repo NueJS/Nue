660f65ac63d8a0131d611a0d3cd0ca229b0f6753
Manan Tank <manantankm@gmail.com>
2021-02-26 21:25:48 +0530

    remove: traverse.js

--- src/utils/node/traverse.js
+++
@@ -1,8 +0,0 @@
-// recursively traverse the node and call cb for each of its child
-// if ignore is true, ignore the root node
-const traverse = (node, cb, ignoreRoot = false) => {
-  if (!ignoreRoot) cb(node)
-  if (node.hasChildNodes()) node.childNodes.forEach(n => traverse(n, cb))
-}
-
-export default traverse
