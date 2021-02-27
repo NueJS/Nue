891c17c6393d0db5cac3363b3368a2fb1984bf4e
Manan Tank <manantankm@gmail.com>
2021-02-28 03:28:53 +0530

    remove old from onMutate

--- src/utils/state/onMutate.js
+++ src/utils/state/onMutate.js
@@ -5,12 +5,12 @@ import { triggerDeps } from '../callbacks.js'
 // triggering callbacks in beforeUpdate and afterUpdate may mutate the state further
 // which would trigger some more callbacks, to avoid calling the same cb more than once, build a batches
 // once a cb is added in batches it is not added again
-const onMutate = (nue, path, old) => {
+const onMutate = (nue, path) => {
   // if batches is being built, don't invoke, invoke once the building is stopped
   if (!nue.batching) flush(nue)
 
   // don't use if else - value of nue.batching will be changed by flush
-  if (nue.batching) triggerDeps(nue, path, old)
+  if (nue.batching) triggerDeps(nue, path)
 }
 
 export default onMutate
