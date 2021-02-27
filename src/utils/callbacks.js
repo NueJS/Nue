f531bb66336f0ab7763bd0da4ac81fa7766192d6
Manan Tank <manantankm@gmail.com>
2021-02-28 03:26:44 +0530

    remove info from trigger functions

--- src/utils/callbacks.js
+++ src/utils/callbacks.js
@@ -1,18 +1,18 @@
 // only trigger cbs in $ Map
-export const trigger$Cbs = (nue, target, info) => {
-  target.$.forEach(cb => cb(nue, info))
+export const trigger$Cbs = (nue, target) => {
+  target.$.forEach(cb => cb())
 }
 
 // trigger all the cbs in the object
-export const triggerAllCbs = (nue, target, info) => {
+export const triggerAllCbs = (nue, target) => {
   for (const k in target) {
-    if (k === '$') trigger$Cbs(nue, target, info)
-    else triggerAllCbs(nue, target[k], info)
+    if (k === '$') trigger$Cbs(nue, target)
+    else triggerAllCbs(nue, target[k])
   }
 }
 
 // find callbacks that are subscribed to given path and trigger them
-export const triggerDeps = (nue, path, info) => {
+export const triggerDeps = (nue, path) => {
   let target = nue.subscriptions
   path.forEach((c, i) => {
     // if primitive, return
@@ -20,9 +20,9 @@ export const triggerDeps = (nue, path, info) => {
     target = target[c]
     if (target) {
       // if last index, trigger cbs of target.$
-      if (i !== path.length - 1) trigger$Cbs(nue, target, info)
+      if (i !== path.length - 1) trigger$Cbs(nue, target)
       // deeply trigger all inside target.*
-      triggerAllCbs(nue, target, info)
+      triggerAllCbs(nue, target)
     }
   })
 }
