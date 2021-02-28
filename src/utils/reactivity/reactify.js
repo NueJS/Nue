25916433fb7b94fb6c36b71b8f76df8388912d6f
Manan Tank <manantankm@gmail.com>
2021-02-28 23:45:39 +0530

    refactor reactify - update path when reactive object's key changes

--- src/utils/reactivity/reactify.js
+++ src/utils/reactivity/reactify.js
@@ -2,16 +2,15 @@ import onMutate from '../state/onMutate.js'
 import modes from './modes.js'
 import computedState from '../state/computedState.js'
 import { accessed } from '../state/detectStateUsage.js'
-import deepEqual from '../deepEqual.js'
-import { DETECTIVE_MODE, NO_OVERRIDE_MODE, REACTIVE_MODE, TARGET } from '../constants.js'
-
-const isObject = x => typeof x === 'object' && x !== null
+import { DETECTIVE_MODE, IS_REACTIVE, NO_OVERRIDE_MODE, REACTIVE_MODE, TARGET, UPDATE_INDEX } from '../constants.js'
+import { isObject } from '../others.js'
 
 // create a reactive object which when mutated calls the on_change function
-const reactify = (nue, obj, path = [], closure$) => {
+const reactify = (nue, obj, _path = [], closure$) => {
+  let path = _path
   if (!isObject(obj)) return obj
 
-  // make the slice of state reactive
+  // make the child objects reactive
   const target = Array.isArray(obj) ? [] : {}
   Object.keys(obj).forEach(key => {
     target[key] = reactify(nue, obj[key], [...path, key])
@@ -19,13 +18,19 @@ const reactify = (nue, obj, path = [], closure$) => {
 
   const reactive = new Proxy(target, {
 
-    has (target, property) {
-      // check if the property is in target or its closure
-      return property in target || (closure$ ? property in closure$ : false)
+    // return true if the prop is in target or its closure
+    has (target, prop) {
+      return prop in target || (closure$ ? prop in closure$ : false)
     },
 
     set (target, prop, newValue) {
-      let success
+      // change the reactive object's path, because it has been moved to a different key
+      if (prop === UPDATE_INDEX) {
+        // newValue is the index at which the reactive is moved
+        path = [...path.slice(0, -1), newValue]
+        return true
+      }
+
       // if the mutated prop exists in the target already
       const propInTarget = prop in target
 
@@ -44,20 +49,32 @@ const reactify = (nue, obj, path = [], closure$) => {
         return Reflect.set(closure$, prop, newValue)
       }
 
-      if (isObject(value) && !value.__reactive__) {
-        value = reactify(nue, value, [...path, prop])
+      if (isObject(value)) {
+        // if value is not reactive, make it reactive
+        if (!value[IS_REACTIVE]) value = reactify(nue, value, [...path, prop])
+        // when a reactive value is set on some index(prop) in target array
+        // we have to update that reactive object's path - because we are changing the index it was created at
+        else if (Array.isArray(target)) value[UPDATE_INDEX] = prop
       }
+
+      // -----------------------------
+      const set = () => Reflect.set(target, prop, value)
 
       if (modes[REACTIVE_MODE]) {
-        if (!(prop === 'length' && Array.isArray(target))) {
-          if (!deepEqual(target[prop], value)) {
-            success = Reflect.set(target, prop, value)
-            onMutate(nue, [...path, prop])
-          }
+        // push to batchInfo and call onMutate
+        const oldValue = target[prop]
+        const newValue = value
+        const success = set()
+        if (oldValue !== newValue) {
+          const mutatedPath = [...path, prop]
+          nue.batchInfo.push({ oldValue, newValue, path: mutatedPath })
+          onMutate(nue, mutatedPath)
         }
+
+        return success
       }
 
-      return success || Reflect.set(target, prop, value)
+      return set()
     },
 
     deleteProperty (target, prop) {
@@ -66,7 +83,7 @@ const reactify = (nue, obj, path = [], closure$) => {
     },
 
     get (target, prop) {
-      if (prop === '__reactive__') return true
+      if (prop === IS_REACTIVE) return true
       if (prop === TARGET) return target
       if (modes[DETECTIVE_MODE]) {
         if (path.length !== 0) accessed.paths[accessed.paths.length - 1] = [...path, prop]
