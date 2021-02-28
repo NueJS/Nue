e94148e1b4421fd8c8ff0f2c68fba00735b39ed8
Manan Tank <manantankm@gmail.com>
2021-02-28 23:43:55 +0530

    turn off symbol-description in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   rules: {
     'brace-style': 'off',
     'no-throw-literal': 'off',
-    'spaced-comment': 'off'
+    'spaced-comment': 'off',
+    'symbol-description': 'off'
   }
 }
