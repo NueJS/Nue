463a8ed9a7fee423b783d6b1994a82e80b49c347
Manan Tank <manantankm@gmail.com>
2021-03-08 14:30:04 +0530

    remove hasSlice()

--- src/utils/state/slice.js
+++ src/utils/state/slice.js
@@ -1,18 +1,5 @@
-import { isDefined } from '../others'
-
 export const targetProp = (obj, path) => {
   const target = path.slice(0, -1).reduce((target, key) => target[key], obj)
   const prop = path[path.length - 1]
   return [target, prop]
-}
-
-export const hasSlice = (obj, path) => {
-  let target = obj
-
-  for (const p of path) {
-    if (!(p in target)) return false
-    target = target[p]
-  }
-
-  return isDefined(target)
 }
