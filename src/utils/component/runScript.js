a764d5e58de98235054376ee0942d1eda015a8e8
Manan Tank <manantankm@gmail.com>
2021-03-08 19:08:33 +0530

    minor refactor

--- src/utils/component/runScript.js
+++ src/utils/component/runScript.js
@@ -4,8 +4,10 @@ import modes from '../reactivity/modes.js'
 const runScript = (compNode, script) => {
   modes[REACTIVE_MODE] = false
   modes[NO_OVERRIDE_MODE] = true
+
   const { $, refs, fn, events } = compNode
   script({ $, refs, fn, ...events, events })
+
   modes[REACTIVE_MODE] = true
   modes[NO_OVERRIDE_MODE] = false
 }
