daa8fa4c6e331bba1882250ccea9f95e4cd3056a
Manan Tank <manantankm@gmail.com>
2021-03-13 22:02:48 +0530

    add type info in stats.js

--- src/utils/stats.js
+++ src/utils/stats.js
@@ -1,7 +1,10 @@
-export default {
+const stats = {
+  /** @type Record<string, Function> */
   components: {},
   error: false,
   config: {
     defaultStyle: ':host{display:block;}'
   }
 }
+
+export default stats
