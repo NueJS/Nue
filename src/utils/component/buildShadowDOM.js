2a75dabef50619b5ae0e7c7a0785d7a39f3493c4
Manan Tank <manantankm@gmail.com>
2021-03-14 00:07:54 +0530

    add types to buildShadowDOM

--- src/utils/component/buildShadowDOM.js
+++ src/utils/component/buildShadowDOM.js
@@ -3,6 +3,11 @@ import getClone from '../node/clone.js'
 import { flushArray } from '../others.js'
 import processNode from '../process/processNode.js'
 
+/**
+ * process templateNode and add it in shadowDOM of compNode
+ * @param {import('../types').compNode} compNode
+ * @param {Element} templateNode
+ */
 const buildShadowDOM = (compNode, templateNode) => {
   // clone templateNode
   const fragment = getClone(templateNode.content)
@@ -12,7 +17,9 @@ const buildShadowDOM = (compNode, templateNode) => {
   flushArray(compNode[DEFERRED_WORK])
   // create shadowRoot
   compNode.attachShadow({ mode: 'open' })
+
   // add fragment nodes to shadowRoot
+  // @ts-ignore (shadowRoot will not be null because mode is open)
   compNode.shadowRoot.append(fragment)
 }
 
