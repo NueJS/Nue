696d6e79f2e93471e3bafc53502b58cbaa0038a0
Manan Tank <manantankm@gmail.com>
2021-03-13 23:58:53 +0530

    add types in runScript

--- src/utils/component/runScript.js
+++ src/utils/component/runScript.js
@@ -2,23 +2,45 @@ import { NO_OVERRIDE_MODE, REACTIVE_MODE } from '../constants.js'
 import modes from '../reactivity/modes.js'
 import { dashifyComponentNames } from '../string/dashify.js'
 
+/** @typedef {import('../types').compNode} compNode */
+
+/**
+ *
+ * @param {Array<string>} strings
+ * @param  {...string} exprs
+ * @returns
+ */
 const tag = (strings, ...exprs) => exprs.reduce((acc, expr, i) => acc + strings[i] + expr, '') + strings[strings.length - 1]
 
+/**
+ *
+ * @param {compNode} compNode
+ * @param {Function} component
+ * @param {boolean} parsed
+ * @returns {[string, string, Array<Function>]}
+ */
 const runScript = (compNode, component, parsed) => {
+  /** @type {Array<Function>} */
   let childComponents = []
   let templateString = ''
   let cssString = ''
 
+  /** @param {...string} args */
   const html = (...args) => {
     if (parsed) return
+    // @ts-expect-error
     templateString = tag(...args)
   }
 
+  /** @param {...string} args */
   const css = (...args) => {
     if (parsed) return
+
+    // @ts-expect-error
     cssString = tag(...args)
   }
 
+  /** @param {...Function} _childComponents */
   const components = (..._childComponents) => {
     if (parsed) return
     childComponents = _childComponents
@@ -33,7 +55,11 @@ const runScript = (compNode, component, parsed) => {
   modes[REACTIVE_MODE] = true
   modes[NO_OVERRIDE_MODE] = false
 
-  return [dashifyComponentNames(templateString, childComponents), cssString, childComponents]
+  return [
+    dashifyComponentNames(templateString, childComponents),
+    cssString,
+    childComponents
+  ]
 }
 
 export default runScript
