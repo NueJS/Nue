be862d109d1b906fdaed663fae0f2f00cdfe062b
Manan Tank <manantankm@gmail.com>
2021-03-13 23:52:10 +0530

    add types in dashify.js

--- src/utils/string/dashify.js
+++ src/utils/string/dashify.js
@@ -1,5 +1,16 @@
-export const dashify = s => s + '-'
+/**
+ * add dash at the end of string
+ * @param {string} str
+ * @returns {string}
+ */
+export const dashify = str => str + '-'
 
+/**
+ * replace component names in html with dashed names
+ * @param {string} html
+ * @param {Array<Function>} components
+ * @returns {string}
+ */
 export const dashifyComponentNames = (html, components) =>
   components.reduce(
     (acc, comp) => acc.replace(new RegExp(`<${comp.name}|</${comp.name}`, 'g'), dashify),
