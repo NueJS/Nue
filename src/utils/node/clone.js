2eddc7d7e5bdd079479d39529cb21cbfb5f27632
Manan Tank <manantankm@gmail.com>
2021-03-14 00:32:08 +0530

    add types in getClone

--- src/utils/node/clone.js
+++ src/utils/node/clone.js
@@ -1,5 +1,10 @@
 import copyParsed from './copyParsed'
 
+/**
+ * clone the node and add the parsed prop
+ * @param {Node} node
+ * @returns {Node}
+ */
 const getClone = (node) => {
   const clone = node.cloneNode(true)
   copyParsed(node, clone)
