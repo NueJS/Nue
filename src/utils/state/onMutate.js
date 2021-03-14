66d6c2a0418fe930be8e619fb7612c070380f027
Manan Tank <manantankm@gmail.com>
2021-03-14 13:36:36 +0530

    add types in onMutate

--- src/utils/state/onMutate.js
+++ src/utils/state/onMutate.js
@@ -2,8 +2,12 @@ import { scheduleFlush } from '../flush.js'
 import notify from '../subscription/notify'
 import { FLUSH_SCHEDULED, SUBSCRIPTIONS } from '../constants.js'
 
-// when state is mutated, add the cb in batch
-// schedule flush if not already scheduled
+/**
+ * when state is mutated, add the cb in batch
+ * schedule flush if not already scheduled
+ * @param {import('../types').compNode} compNode
+ * @param {import('../types').path} path
+ */
 const onMutate = (compNode, path) => {
   notify(compNode[SUBSCRIPTIONS], path)
   if (!compNode[FLUSH_SCHEDULED]) scheduleFlush(compNode)
