8cb7853ae2041967959f7d83ba5fa20142f31c3f
Manan Tank <manantankm@gmail.com>
2021-03-14 15:26:24 +0530

    add types in executeRemove

--- src/utils/process/loop/executeSteps/executeRemove.js
+++ src/utils/process/loop/executeSteps/executeRemove.js
@@ -1,3 +1,9 @@
+/**
+ *
+ * @param {number} index
+ * @param {Array<import("../../../types").compNode>} comps
+ */
+
 const executeRemove = (index, comps) => {
   comps[index].remove()
   comps.splice(index, 1)
