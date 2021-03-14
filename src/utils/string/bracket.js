06367a8119a07c8da13cf566bb9e253bb9f7d3b0
Manan Tank <manantankm@gmail.com>
2021-03-14 16:47:49 +0530

    add types in bracket

--- src/utils/string/bracket.js
+++ src/utils/string/bracket.js
@@ -1,5 +1,13 @@
-// remove first and last character
+/**
+ * remove first and last character
+ * @param {string} str
+ * @returns {string}
+ */
 export const unBracket = str => str.slice(1, -1)
 
-// check if the string has brackets at the ends
+/**
+ * check if the string has brackets at the ends
+ * @param {string} str
+ * @returns {boolean}
+ */
 export const isBracketed = str => str[0] === '[' && str.endsWith(']')
