0948dae13ee5ed53805d0319a7ba964cab64f24d
Manan Tank <manantankm@gmail.com>
2021-03-14 09:40:38 +0530

    add types in parseNode

--- src/utils/parse/parseNode.js
+++ src/utils/parse/parseNode.js
@@ -2,15 +2,27 @@ import parseAttributes from './parseAttributes'
 import parseTextNode from './parseTextNode'
 import parseComp from './parseComp'
 
+/**
+ *
+ * @param {Node} node
+ * @param {Record<string, string>} childCompNodeNames
+ * @param {Array<Function>} deferred
+ */
 const parseNode = (node, childCompNodeNames, deferred) => {
   // if node is component, get it's name else it will be undefined
   const compName = childCompNodeNames[node.nodeName]
+
   // #text
   if (node.nodeType === Node.TEXT_NODE) return parseTextNode(node, deferred)
+
   // component
+  // @ts-ignore
   if (compName) parseComp(node, compName, deferred)
+
   // attributes on component or simple node
+  // @ts-ignore
   if (node.hasAttribute) parseAttributes(node, compName)
+
   // child nodes of component or simple node
   if (node.hasChildNodes()) node.childNodes.forEach(childNode => parseNode(childNode, childCompNodeNames, deferred))
 }
