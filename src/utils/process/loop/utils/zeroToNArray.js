a1e25b4469291aa23b7b8822417aebb57e7d8118
Manan Tank <manantankm@gmail.com>
2021-03-14 19:27:27 +0530

    add types in zeroToNArray

--- src/utils/process/loop/utils/zeroToNArray.js
+++ src/utils/process/loop/utils/zeroToNArray.js
@@ -1,6 +1,11 @@
-const zeroToNArray = (n) => {
+/**
+ * create an array of given size which consists value same as index it is on
+ * @param {number} length - length of array
+ * @returns {Array<number>}
+ */
+const zeroToNArray = (length) => {
   const arr = []
-  for (let i = 0; i < n; i++) arr.push(i)
+  for (let i = 0; i < length; i++) arr.push(i)
   return arr
 }
 
