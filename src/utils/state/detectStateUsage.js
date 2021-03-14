251df9f3227832f2da6f6aca7a4485372beb6f42
Manan Tank <manantankm@gmail.com>
2021-03-14 13:22:15 +0530

    add types in detectStateUsage

--- src/utils/state/detectStateUsage.js
+++ src/utils/state/detectStateUsage.js
@@ -1,19 +1,30 @@
 import { DETECTIVE_MODE } from '../constants.js'
 import modes from '../reactivity/modes.js'
 
+/**
+ * @typedef {import('../types').path} path
+ */
+
 // when detection mode is enabled is records all the keys that are accessed in state
 // if state.a.b and state.c.d.e is accessed it becomes [['a', 'b'], ['c', 'd', 'e']]
+/**
+ * @type {{ paths: Array<path>}}
+ */
 export const accessed = {
   paths: []
 }
 
 // call the function and detect what keys it is using of this.$
 // also get the return value and send it as well
+/**
+ *
+ * @param {Function} fn
+ */
 const detectStateUsage = (fn) => {
   modes[DETECTIVE_MODE] = true
   const returnVal = fn()
   modes[DETECTIVE_MODE] = false
-  const paths = [...accessed.paths] // copy
+  const paths = [...accessed.paths] // shallow clone
   accessed.paths = []
   return [returnVal, paths]
 }
