ad9cd848ac9a8447a4f81c6d0d0072a7d3f194c5
Manan Tank <manantankm@gmail.com>
2021-03-14 15:48:40 +0530

    add types in batch

--- src/utils/batch.js
+++ src/utils/batch.js
@@ -2,12 +2,22 @@ import devtools from '../apis/devtools'
 import { IS_SUBSCRIBED } from './constants'
 import DEV from './dev/DEV'
 
-// return function which when called adds the cb to given batch
+/**
+ * return function which when called adds the cb to given batch
+ * @param {Function} cb
+ * @param {Set<Function>} batch
+ */
 export const batchify = (cb, batch) => () => batch.add(cb)
 
 // @todo reduce the amount of functions in this file - they all are very similar
+/**
+ *
+ * @param {Set<Function>} batch
+ * @param {import('./types').batchInfoArray} batchInfo
+ */
 export const flushBatch = (batch, batchInfo) => {
   batch.forEach(cb => {
+    // @ts-ignore // @todo fix this
     const { node } = cb
     // if cb is for updating a node, only call cb if node is subscribed
     if ((node && node[IS_SUBSCRIBED]) || !node) {
