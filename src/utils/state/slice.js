eb917e54d3c65421b71f8b38157c1705f45f0f50
Manan Tank <manantankm@gmail.com>
2021-03-14 16:49:56 +0530

    add types in slice

--- src/utils/state/slice.js
+++ src/utils/state/slice.js
@@ -1,3 +1,10 @@
+/**
+ * return [target, prop] for given path in object
+ * @param {Record<string, any>} obj
+ * @param {import("../types").path} path
+ * @returns {[Record<string, any>, any]}
+ */
+
 export const targetProp = (obj, path) => {
   const target = path.slice(0, -1).reduce((target, key) => target[key], obj)
   const prop = path[path.length - 1]
