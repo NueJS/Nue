2a86fb13e6a43c2ea295232e0630fe71abf8722b
Manan Tank <manantankm@gmail.com>
2021-03-14 13:58:38 +0530

    add types in addEvent

--- src/utils/process/attributes/addEvent.js
+++ src/utils/process/attributes/addEvent.js
@@ -1,30 +1,26 @@
-
 import DEV from '../../dev/DEV.js'
 import errors from '../../dev/errors.js'
-import stats from '../../stats.js'
 import { addSubscriber } from '../../subscription/node.js'
 
-const addEvent = (compNode, node, [fnName, eventName]) => {
+/**
+ * add event listener on element
+ * @param {import('../../types').compNode} compNode
+ * @param {Element} element
+ * @param {import('../../types').attribute} param2
+ */
+const addEvent = (compNode, element, [fnName, eventName]) => {
   const handler = compNode.fn[fnName]
   if (DEV && !handler) throw errors.METHOD_NOT_FOUND(compNode.name, fnName)
 
-  const { actions } = stats.components[compNode.name]
-  const action = actions && actions[eventName]
-
-  // handler gets called with event and the state of component event is originated from
+  /** @type {EventListener} */
   const _handler = (e) => handler(e, compNode.$)
 
-  let subscriber
-  if (action) subscriber = () => action(node, _handler)
-
-  else {
-    subscriber = () => {
-      node.addEventListener(eventName, _handler)
-      return () => node.removeEventListener(eventName, _handler)
-    }
+  const subscriber = () => {
+    element.addEventListener(eventName, _handler)
+    return () => element.removeEventListener(eventName, _handler)
   }
 
-  addSubscriber(node, subscriber)
+  addSubscriber(element, subscriber)
 }
 
 export default addEvent
