67a6569aaa3d3dccb92a531381add91c3910e7ab
Manan Tank <manantankm@gmail.com>
2021-03-14 15:42:03 +0530

    add types in closure

--- src/utils/closure.js
+++ src/utils/closure.js
@@ -2,8 +2,12 @@ import { ORIGIN_MODE } from './constants'
 import modes from './reactivity/modes'
 import { targetProp } from './state/slice'
 
-// get the origin component where the value of the state is coming from
-// @todo replace with a custom mode in reactify?
+/**
+ * get the origin component where the value of the state is coming from
+ * @param {import('./types').compNode} compNode
+ * @param {import('./types').path} path
+ * @returns {import('./types').compNode}
+ */
 export const origin = (compNode, path) => {
   if (path.length === 0) return compNode
   modes[ORIGIN_MODE] = true
