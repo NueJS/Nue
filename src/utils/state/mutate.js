6292ce73957b63088414de8ab121890b092605b0
Manan Tank <manantankm@gmail.com>
2021-03-14 13:36:25 +0530

    add types in mutate

--- src/utils/state/mutate.js
+++ src/utils/state/mutate.js
@@ -1,8 +1,15 @@
 import { targetProp } from './slice'
 
-const mutate = (obj, path, value, trap = 'set') => {
+/**
+ * mutate the object at given path to newValue
+ * @param {Record<string, any>} obj
+ * @param {import('../types').path} path
+ * @param {any} newValue
+ * @returns
+ */
+const mutate = (obj, path, newValue) => {
   const [target, prop] = targetProp(obj, path)
-  return Reflect[trap](target, prop, value)
+  return Reflect.set(target, prop, newValue)
 }
 
 export default mutate
