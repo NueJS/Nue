90b553765f07a0e2c831e6fee27c5d434a871ea3
Manan Tank <manantankm@gmail.com>
2021-03-14 14:47:19 +0530

    add types on addFn

--- src/utils/process/attributes/addFn.js
+++ src/utils/process/attributes/addFn.js
@@ -1,5 +1,12 @@
-const addFn = (compNode, node, [value, name]) => {
-  compNode.fn[name] = compNode.closure.fn[value]
+/**
+ * add Fn on compo
+ * @param {import("../../types").compNode} parentCompNode
+ * @param {import("../../types").compNode} compNode
+ * @param {import("../../types").attribute} param2
+ */
+const addFn = (parentCompNode, compNode, [value, name]) => {
+  // @ts-ignore
+  compNode.fn[name] = parentCompNode.closure.fn[value]
 }
 
 export default addFn
