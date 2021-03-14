962297c1a78b84a63be1f29162f76e8b90347f6a
Manan Tank <manantankm@gmail.com>
2021-03-14 15:18:39 +0530

    add types on checkUniquenessOfKeys

--- src/utils/process/loop/dev/checkUniquenessOfKeys.js
+++ src/utils/process/loop/dev/checkUniquenessOfKeys.js
@@ -1,11 +1,21 @@
 import errors from '../../../dev/errors'
 
-const checkUniquenessOfKeys = (compNode, keys) => {
-  const isUniqueArray = (arr) => {
-    const set = new Set(arr)
-    return set.size === arr.length
-  }
+/**
+ *
+ * @param {Array<any>} arr
+ * @returns {boolean}
+ */
+const isUniqueArray = (arr) => {
+  const set = new Set(arr)
+  return set.size === arr.length
+}
 
+/**
+ *
+ * @param {import('../../../types').compNode} compNode
+ * @param {Array<string>} keys
+ */
+const checkUniquenessOfKeys = (compNode, keys) => {
   if (!isUniqueArray(keys)) {
     throw errors.KEYS_ARE_NOT_UNIQUE(compNode.name, keys)
   }
