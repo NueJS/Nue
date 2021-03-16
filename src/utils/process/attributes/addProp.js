416d50fd12d366d04414f32dcfcc8606822332f6
Manan Tank <manantankm@gmail.com>
2021-03-16 22:08:38 +0530

    update types in addProp

--- src/utils/process/attributes/addProp.js
+++ src/utils/process/attributes/addProp.js
@@ -1,19 +1,23 @@
+import { FN } from '../../constants.js'
 import mutate from '../../state/mutate.js'
 import { syncNode } from '../../subscription/node.js'
 
 /**
  * add prop on Element
  * @param {import('../../types').compNode} compNode
- * @param {Element} element
+ * @param {import('../../types').parsedElement} element
  * @param {import('../../types').attribute} param2
  */
-const addProp = (compNode, element, [{ getValue, deps }, propName]) => {
+const addProp = (compNode, element, [{ getValue, deps, type }, propName]) => {
   const setProp = () => {
     // @ts-ignore
     element[propName] = getValue(compNode)
   }
 
   if (element.matches('input, textarea')) {
+    if (type === FN) {
+      throw Error('input binding can not be a functional placeholder')
+    }
     // @ts-ignore
     const isNumber = element.type === 'number' || element.type === 'range'
     const handler = () => {
