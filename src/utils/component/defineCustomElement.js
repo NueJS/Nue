6498f929d0181915345acccb66294d69b08cc85f
Manan Tank <manantankm@gmail.com>
2021-03-24 03:05:17 +0530

    update types in defineCustomElement

--- src/utils/component/defineCustomElement.js
+++ src/utils/component/defineCustomElement.js
@@ -1,31 +1,30 @@
-import runScript from './runScript.js'
-import addLifecycles, { runEvent } from './lifecycle.js'
-import buildShadowDOM from './buildShadowDOM.js'
-import processNode from '../process/processNode.js'
-import stats from '../stats'
+import { ITSELF } from '../../constants.js'
+import data from '../data'
 import { createElement } from '../node/dom.js'
 import parseTemplate from '../parse/parseTemplate'
-import { BATCH_INFO, BEFORE_DOM_BATCH, DEFERRED_WORK, DOM_BATCH, IGNORE_DISCONNECT, INIT_$, ITSELF, NODES_USING_CLOSURE, ON_DESTROY_CBS, ON_MOUNT_CBS, PARSED, NODES_USING_STATE, REORDERING, SUBSCRIPTIONS } from '../constants.js'
-import processAttributes from '../process/attributes/processAttributes.js'
 import reactify from '../reactivity/reactify.js'
 import { subscribeNode, unsubscribeNode } from '../subscription/node.js'
 import { dashify } from '../string/dashify.js'
 import { upper } from '../others.js'
+import runComponent from './runComponent.js'
+import addLifecycles from './lifecycle.js'
+import buildShadowDOM from './buildShadowDOM.js'
+// import process from '../hydration/hydrate.js'
+// import processAttributes from '../process/attributes/processAttributes.js'
 
 /**
- * defines a custom element using the component function
- * @param {Function} component
+ * defines a custom element using the compFn function
+ * @param {Function} compFn
  */
+const defineCustomElement = (compFn) => {
+  const { __components, __config } = data
 
-const defineCustomElement = (component) => {
-  const { components, config } = stats
+  // use the function's name as the compFn's name
+  const { name } = compFn
 
-  // use the function's name as the component's name
-  const { name } = component
-
-  // return early if the component with this name is already defined
-  if (name in components) return
-  components[name] = component
+  // return early if the compFn with this name is already defined
+  if (name in __components) return
+  __components[name] = compFn
 
   /** @type {HTMLTemplateElement}*/
   let componentTemplateElement
@@ -33,66 +32,67 @@ const defineCustomElement = (component) => {
   class NueComp extends HTMLElement {
     constructor () {
       super()
-      /** @type {import('../types').compNode} */
+      /** @type {import('types/dom').Comp} */
       // @ts-expect-error
-      const compNode = this
-      // name of the custom element
-      compNode.name = name
+      const comp = this
+
+      comp.__compFnName = name
+
       // refs of child nodes with *ref='ref-name' attribute
-      compNode.refs = {}
+      comp.refs = {}
+
       // subscription tree which contains the callbacks stored at various dependency paths
-      compNode[SUBSCRIPTIONS] = { [ITSELF]: new Set() }
+      comp.__subscriptions = { [ITSELF]: new Set() }
 
-      // batches
-      compNode[BEFORE_DOM_BATCH] = new Set()
-      compNode[DOM_BATCH] = new Set()
+      // @ts-expect-error
+      comp.__batches = [new Set(), new Set()]
 
-      // Array of mutation info that happened in a flush
-      compNode[BATCH_INFO] = []
+      // Array of mutation info that happened in a batch
+      comp.__mutations = []
 
       // array of callbacks that should be run after some process is done
-      compNode[DEFERRED_WORK] = []
+      comp.__deferredWork = []
 
       // nodes that are using the state
-      compNode[NODES_USING_STATE] = new Set()
+      comp.__nodesUsingLocalState = new Set()
 
       // nodes that are using the closure state
-      compNode[NODES_USING_CLOSURE] = new Set()
+      comp.__nodesUsingClosureState = new Set()
 
-      if (!compNode[INIT_$]) compNode[INIT_$] = {}
+      if (!comp.__prop$) comp.__prop$ = {}
 
-      addLifecycles(compNode)
+      addLifecycles(comp)
     }
 
     connectedCallback () {
-      /** @type {import('../types').compNode} */
+      /** @type {import('types/dom').Comp} */
       // @ts-expect-error
-      const compNode = this
+      const comp = this
 
-      // if the connection change is due to reordering, ignore
-      if (compNode[REORDERING]) return
+      if (comp.__moving) return
 
-      // do not ignore disconnect
-      compNode[IGNORE_DISCONNECT] = false
+      comp.__manuallyDisconnected = false
 
-      // when component is being connected for the first time
-      if (!compNode.shadowRoot) {
-        const { closure } = compNode
-        // add fn
-        compNode.fn = closure ? Object.create(closure.fn) : {}
-        // add $
-        const closure$ = closure && closure.$
-        compNode.$ = reactify(compNode, compNode[INIT_$], [], closure$)
+      // when compFn is being connected for the first time
+      if (!comp.shadowRoot) {
+        const { parent } = comp
+
+        // create fn
+        comp.fn = parent ? Object.create(parent.fn) : {}
+
+        // create $
+        comp.$ = reactify(comp, [])
 
         // process attributes
-        if (compNode[PARSED]) {
-          const { attributes } = compNode[PARSED]
-          if (attributes) processAttributes(compNode, compNode, attributes)
-        }
+        // do this only on looped components - right ?
+        // if (comp.__parsedInfo) {
+        //   const { __attributes } = comp.__parsedInfo
+        //   if (__attributes) processAttributes(comp, comp, __attributes)
+        // }
 
-        const [templateString, cssString, childComponents] = runScript(compNode, component, !!componentTemplateElement)
+        const [templateString, cssString, childComponents] = runComponent(comp, compFn, !!componentTemplateElement)
 
-        // do this only once per component ------------------
+        // do this only once per compFn ------------------
         if (!componentTemplateElement) {
           /** @type {Record<string, string>} */
           let childCompNodeNames = {}
@@ -114,59 +114,56 @@ const defineCustomElement = (component) => {
           // create componentTemplateElement using template, style, and defaultStyle
           // @ts-ignore
           componentTemplateElement = createElement('template')
-          componentTemplateElement.innerHTML = templateString + `<style default> ${config.defaultStyle} </style>` + '<style scoped >' + cssString + '</style>'
+          componentTemplateElement.innerHTML = templateString + `<style default> ${__config.defaultStyle} </style>` + '<style scoped >' + cssString + '</style>'
 
           // parse the template and create componentTemplateElement which has all the parsed info
-          parseTemplate(compNode, componentTemplateElement, childCompNodeNames)
+          parseTemplate(comp, componentTemplateElement, childCompNodeNames)
 
           childComponents.forEach(defineCustomElement)
         }
 
         // process childNodes (DOM) and shadow DOM
-        // TODO: processNode should be able to take the fragment node
-        compNode.childNodes.forEach(node =>
+        // TODO: process should be able to take the fragment node
+        comp.childNodes.forEach(node =>
           // @ts-ignore
-          processNode(compNode, node)
+          process(comp, node)
         )
-        buildShadowDOM(compNode, componentTemplateElement)
+        buildShadowDOM(comp, componentTemplateElement)
 
-        // connect all nodes using state (local + closure)
-        compNode[NODES_USING_STATE].forEach(subscribeNode)
+        // connect all nodes using local state
+        comp.__nodesUsingLocalState.forEach(subscribeNode)
 
         // subscribe node, so that it's attributes are in sync
-        subscribeNode(compNode)
+        subscribeNode(comp)
       }
 
       // only connect nodes that were previously disconnected (nodes using closure state)
       else {
-        compNode[NODES_USING_CLOSURE].forEach(subscribeNode)
+        comp.__nodesUsingClosureState.forEach(subscribeNode)
       }
 
-      // run onMount callbacks
-      runEvent(compNode, ON_MOUNT_CBS, compNode[BATCH_INFO])
+      comp.__lifecycleCallbacks.__onMount.forEach(cb => cb())
     }
 
     disconnectedCallback () {
-      /** @type {import('../types').compNode} */
+      /** @type {import('types/dom').Comp} */
       // @ts-expect-error
-      const compNode = this
-      // if disconnectedCallback was manually called earlier, no need to call it again when node is removed
-      if (compNode[IGNORE_DISCONNECT]) return
+      const comp = this
 
-      // do nothing, if the connection change is due to reordering
-      if (compNode[REORDERING]) return
+      if (comp.__manuallyDisconnected) return
+      if (comp.__moving) return
 
       // run onDestroy callbacks
-      runEvent(compNode, ON_DESTROY_CBS, compNode[BATCH_INFO])
+      comp.__lifecycleCallbacks.__onDestroy.forEach(cb => cb())
 
       // only disconnect nodes that are using closure, no need to disconnect nodes that use local state only
-      compNode[NODES_USING_CLOSURE].forEach(unsubscribeNode)
+      comp.__nodesUsingClosureState.forEach(unsubscribeNode)
 
-      // unsubscribeNode(compNode) (not needed ?)
+      // unsubscribeNode(comp) (not needed ?)
     }
   }
 
-  // define current component and then it's children
+  // define current compFn and then it's children
   customElements.define(dashify(name), NueComp)
 }
 
