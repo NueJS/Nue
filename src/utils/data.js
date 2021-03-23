60854208bb63d24a2663fe9edd3039b2b1df4d74
Manan Tank <manantankm@gmail.com>
2021-03-24 02:15:16 +0530

    use mangled props on data, add __onNodeUpdate

---
+++ src/utils/data.js
@@ -0,0 +1,12 @@
+const data = {
+  /** @type Record<string, Function> */
+  __components: {},
+  __errorThrown: false,
+  __config: {
+    defaultStyle: ':host{display:block;}'
+  },
+  /** @type {Function | undefined} */
+  __onNodeUpdate: undefined
+}
+
+export default data
