1ae5fb21d215f78439c83031d84ab8714d37654b
Manan Tank <manantankm@gmail.com>
2021-03-26 00:46:29 +0530

    fix copyParsed

--- src/utils/node/copyParsed.js
+++ src/utils/node/copyParsed.js
@@ -1,24 +1,19 @@
 // copy .parsed properties from node's tree to cloneNode's tree
 // cloneNode is clone of node but it does not have custom .parsed properties added in node's tree
 
-import { PARSED } from '../constants'
-
 /**
- * @param {Node} node
- * @param {Node} cloneNode
+ * @param {import("types/dom").ParsedDOMElement} node
+ * @param {import("types/dom").ParsedDOMElement} cloneNode
  */
-const copyParsed = (node, cloneNode) => {
-  if (PARSED in node) {
-    // @todo if parsed is not messed with - no need to shallow clone
-    // @ts-ignore
-    cloneNode[PARSED] = { ...node[PARSED] }
-  }
+export const copyParsed = (node, cloneNode) => {
+  if (node._parsedInfo) cloneNode._parsedInfo = node._parsedInfo
 
   if (node.hasChildNodes()) {
     node.childNodes.forEach((childNode, i) => {
-      copyParsed(childNode, cloneNode.childNodes[i])
+      copyParsed(
+        // @ts-expect-error
+        childNode,
+        cloneNode.childNodes[i])
     })
   }
 }
-
-export default copyParsed
