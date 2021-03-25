0109a59adca64d328d2f5f31dc44e3d3b7a31ea4
Manan Tank <manantankm@gmail.com>
2021-03-25 22:53:48 +0530

    fix executeSteps

---
+++ src/utils/hydration/loop/executeSteps/executeSteps.js
@@ -0,0 +1,16 @@
+import { executeCreate } from './executeCreate.js'
+import { executeRemove } from './executeRemove.js'
+import { executeSwap } from './executeSwap.js'
+
+/**
+ * get the steps from reconcile and perform them in DOM
+ * @param {import('types/reconcile').ReconcileSteps} steps
+ * @param {import('types/loop').LoopInfo} loopInfo
+ */
+export const executeSteps = (steps, loopInfo) => {
+  const { _remove, _add, _swap } = steps
+  const { _loopedCompInstances } = loopInfo
+  _remove.forEach(removeStep => executeRemove(removeStep, _loopedCompInstances))
+  _add.forEach(addStep => executeCreate(addStep, loopInfo))
+  _swap.forEach(swapStep => executeSwap(swapStep, _loopedCompInstances))
+}
