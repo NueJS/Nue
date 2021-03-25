7777defc938b1db0131511f8ab913926500bcfbd
Manan Tank <manantankm@gmail.com>
2021-03-25 21:56:29 +0530

    fix imports of render

--- src/apis/render.js
+++ src/apis/render.js
@@ -1,6 +1,6 @@
-import defineCustomElement from '../utils/component/defineCustomElement'
-import showError from '../utils/dev/error-overlay/showError.js'
-import data from '../utils/data'
+import { defineCustomElement } from '../utils/component/defineCustomElement'
+import { showErrorOverlay } from '../utils/dev/error-overlay/showError.js'
+import { data } from '../utils/data'
 import { dashify } from '../utils/string/dashify'
 
 /**
@@ -9,14 +9,14 @@ import { dashify } from '../utils/string/dashify'
  * @param {HTMLElement} targetElement
  * @param {import('types/others').Config} [config]
  */
-const render = (component, targetElement, config) => {
+export const render = (component, targetElement, config) => {
   // attach error-overlay
   if (_DEV_) {
     // @ts-ignore
     window.data = data
     window.onerror = (message, filename, lineno, colno, error) => {
       // @ts-ignore
-      showError(error)
+      showErrorOverlay(error)
     }
   }
 
@@ -29,5 +29,3 @@ const render = (component, targetElement, config) => {
   const customElement = document.createElement(dashify(component.name))
   targetElement.replaceWith(customElement)
 }
-
-export default render
