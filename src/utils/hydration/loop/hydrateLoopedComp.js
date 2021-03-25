77455dc2ffcfb5ec679c9d48459a2e755c2015d9
Manan Tank <manantankm@gmail.com>
2021-03-26 00:26:35 +0530

    fix hydrateLoopedComp

--- src/utils/hydration/loop/hydrateLoopedComp.js
+++ src/utils/hydration/loop/hydrateLoopedComp.js
@@ -1,21 +1,21 @@
-import subscribe from '../../subscription/subscribe.js'
+import { subscribe } from '../../subscription/subscribe.js'
 import { createComment } from '../../node/dom.js'
-import handleArrayChange from './utils/handleArrayChange.js'
+import { handleArrayChange } from './utils/handleArrayChange.js'
 import { arraysAreShallowEqual } from '../../others.js'
-import getPartialMutationInfo from './utils/getPartialMutationInfo.js'
-import zeroToNArray from './utils/zeroToNArray.js'
+import { getArrayMutationInfo } from './utils/getArrayMutationInfo.js'
+import { zeroToNArray } from './utils/zeroToNArray.js'
+import { batches } from 'enums.js'
 
 /**
- *
- * @param {import('types/dom').Comp} compNode
- * @param {import('types/types.js').compNode} loopedComp
- * @param {import('types/types.js').Parsed} parsed
+ * hydrate looped comp
+ * @param {import('types/dom').LoopedComp} loopedComp
+ * @param {import('types/dom').Comp} parentComp
  */
-const processLoop = (compNode, loopedComp, parsed) => {
-  /** @type {import('types/types.js').forInfo} */
-  // @ts-ignore
-  const forInfo = parsed.for
-  const { itemArray, itemIndex, item, key } = forInfo
+export const hydrateLoopedComp = (loopedComp, parentComp) => {
+  const parsed = loopedComp._parsedInfo
+
+  const loopAttributes = parsed._loopAttributes
+  const { _itemArray, _itemIndex, _item, _key } = loopAttributes
 
   /**
    * return the closure containing value and index
@@ -24,15 +24,17 @@ const processLoop = (compNode, loopedComp, parsed) => {
    * @returns {Record<string, any>}
    */
 
-  const getClosure = (value, index) => ({
-    // @ts-ignore
-    [itemIndex]: index,
-    [item]: value
-  })
+  const getClosure = (value, index) => {
+    const closure = {
+      [_item]: value
+    }
 
-  /**  @returns {Array<any>} */
-  // @ts-ignore
-  const getArray = () => itemArray.getValue(compNode)
+    if (_itemIndex) closure[_itemIndex] = index
+    return closure
+  }
+
+  /** @returns {Array<any>} */
+  const getArray = () => _itemArray._getValue(loopedComp.$, loopedComp._compFnName)
 
   /**
    * return the value of key using the closure information
@@ -40,57 +42,57 @@ const processLoop = (compNode, loopedComp, parsed) => {
    * @param {number} index
    * @returns {any}
    */
-  // @ts-ignore
-  const getKey = (value, index) => key.getValue({ name: parsed.name, $: getClosure(value, index) })
+
+  // @todo current key can only be from closure, add support for state too
+  const getKey = (value, index) => _key._getValue(getClosure(value, index), loopedComp._compFnName)
 
   const getKeys = () => getArray().map(getKey)
 
   // @ts-ignore
-  const arrayPath = itemArray.deps[0]
+  const arrayPath = _itemArray.deps[0]
   const arrayPathString = arrayPath.join('.')
-  const anchorNode = createComment('loop/')
+  const anchor = createComment('loop/')
 
-  const oldState = { values: [], keys: [], keyHash: {} }
+  const oldState = { _values: [], _keys: [], _keyHash: {} }
 
-  /** @type {import('types/types.js').loopInfo} */
-  const blob = {
-    comps: [],
-    anchorNode,
-    ...forInfo,
-    loopedComp,
-    getArray,
-    getClosure,
-    getKeys,
-    compNode,
-    initialized: false
+  /** @type {import('types/loop').LoopInfo} */
+  const loopInfo = {
+    _loopedCompInstances: [],
+    _anchor: anchor,
+    _loopedComp: loopedComp,
+    _getArray: getArray,
+    _getClosure: getClosure,
+    _getKeys: getKeys,
+    _parentComp: parentComp,
+    _instanciated: false,
+    _loopAttributes: loopAttributes,
+    _animation: parsed._animationAttributes
   }
 
   const fullReconcile = () => {
     const n = getArray().length
-    handleArrayChange(blob, zeroToNArray(n), {}, oldState)
+    handleArrayChange(loopInfo, zeroToNArray(n), {}, oldState)
   }
 
-  compNode[DEFERRED_WORK].push(() => {
-    loopedComp.before(anchorNode)
+  parentComp._deferredWork.push(() => {
+    loopedComp.before(anchor)
     loopedComp.before(createComment('/loop'))
     loopedComp.remove()
     fullReconcile()
-    blob.initialized = true
+    loopInfo._instanciated = true
   })
 
-  /** @type {import('types/types.js').subscribeCallback} */
-  const onDepsChange = (batchInfoArray) => {
+  /** @type {import('types/subscription').SubCallBack} */
+  const onDepsChange = (mutations) => {
     // if some mutation in batch assigned a new array
-    const newArrayAssigned = batchInfoArray.some(batchInfo => arraysAreShallowEqual(batchInfo.path, arrayPath))
+    const newArrayAssigned = mutations.some(batchInfo => arraysAreShallowEqual(batchInfo.path, arrayPath))
     if (newArrayAssigned) fullReconcile()
     else {
       // partial reconciliation
-      const [dirtyIndexes, stateUpdatePaths] = getPartialMutationInfo(batchInfoArray, arrayPathString, arrayPath)
-      handleArrayChange(blob, dirtyIndexes, stateUpdatePaths, oldState)
+      const [dirtyIndexes, stateUpdatePaths] = getArrayMutationInfo(mutations, arrayPathString, arrayPath)
+      handleArrayChange(loopInfo, dirtyIndexes, stateUpdatePaths, oldState)
     }
   }
 
-  subscribe(compNode, arrayPath, onDepsChange, DOM_BATCH)
+  subscribe(parentComp, arrayPath, onDepsChange, batches._DOM)
 }
-
-export default processLoop
