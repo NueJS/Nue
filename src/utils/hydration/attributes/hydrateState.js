284b5b83a12227182da309bb09a202fa1a5c1786
Manan Tank <manantankm@gmail.com>
2021-03-25 17:39:37 +0530

    update types in hydrateState

---
+++ src/utils/hydration/attributes/hydrateState.js
@@ -0,0 +1,27 @@
+import { batches } from 'enums'
+import { subscribeMultiple } from '../../subscription/subscribe'
+
+/**
+ * add state on target
+ * @param {import('types/dom').Comp} target
+ * @param {import('types/parsed').Attribute_ParseInfo} attribute
+ * @param {import('types/dom').Comp} comp
+ */
+const addState = (target, attribute, comp) => {
+  const { _placeholder, _name } = attribute
+  const { _getValue, _stateDeps } = /** @type {import('types/placeholder').Placeholder}*/(_placeholder)
+
+  const update = () => {
+    if (target === comp) {
+      target.$[_name] = _getValue(comp)
+    } else {
+      if (!target._prop$) target._prop$ = {}
+      target._prop$[_name] = _getValue(comp)
+    }
+  }
+
+  update()
+  subscribeMultiple(comp, _stateDeps, update, batches._beforeDOM)
+}
+
+export default addState
