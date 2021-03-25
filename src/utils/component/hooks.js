a292a309be16f1387008bf7f0a2e4bebdb624e99
Manan Tank <manantankm@gmail.com>
2021-03-25 17:23:52 +0530

    add hooks.js

---
+++ src/utils/component/hooks.js
@@ -0,0 +1,35 @@
+import { subscribeMultiple } from '../subscription/subscribe.js'
+import errors from '../dev/errors.js'
+
+/**
+ * add lifecycle hooks to comp
+ * @param {import('types/dom').Comp} comp
+ */
+const addHooks = (comp) => {
+  comp._hookCbs = {
+    _onMount: [],
+    _onDestroy: [],
+    _beforeUpdate: [],
+    _afterUpdate: []
+  }
+
+  comp.hooks = {
+    onMount: (cb) => comp._hookCbs._onMount.push(cb),
+    onDestroy: (cb) => comp._hookCbs._onDestroy.push(cb),
+    beforeUpdate: (cb) => comp._hookCbs._beforeUpdate.push(cb),
+    afterUpdate: (cb) => comp._hookCbs._afterUpdate.push(cb),
+
+    onMutate: (cb, ...slices) => {
+      if (_DEV_ && !slices.length) {
+        throw errors.MISSING_DEPENDENCIES_IN_ON_MUTATE(comp._compFnName)
+      }
+
+      comp._hookCbs._onMount.push(() => {
+        const stateDeps = slices.map(slice => slice.split('.'))
+        subscribeMultiple(comp, stateDeps, cb, 0)
+      })
+    }
+  }
+}
+
+export default addHooks
