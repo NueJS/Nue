54b0ae6eb137ae665bc929888c7b8ce40011f427
Manan Tank <manantankm@gmail.com>
2021-03-25 22:01:12 +0530

    fix nodeUpdated

--- src/utils/dev/nodeUpdated.js
+++ src/utils/dev/nodeUpdated.js
@@ -1,11 +1,9 @@
-import data from 'utils/data'
+import { data } from 'utils/data'
 
 /**
  * call this function to show that given node is updated
  * @param {import('types/dom').ParsedDOMElement} node
  */
-const nodeUpdated = (node) => {
+export const nodeUpdated = (node) => {
   if (data._onNodeUpdate) data._onNodeUpdate(node)
 }
-
-export default nodeUpdated
