4a1338fbafc8a4de5bf45498eb2ed6827aff369e
Manan Tank <manantankm@gmail.com>
2021-03-25 22:46:44 +0530

    fix checkUniquenessOfKeys

--- src/utils/hydration/loop/dev/checkUniquenessOfKeys.js
+++ src/utils/hydration/loop/dev/checkUniquenessOfKeys.js
@@ -1,14 +1,12 @@
-import errors from '../../../dev/errors'
+import { errors } from '../../../dev/errors'
 
 /**
  * DEV ONLY: check if all the keys are unique, else throw error
  * @param {string[]} keys
  * @param {import('types/dom').Comp} comp
  */
-const checkUniquenessOfKeys = (keys, comp) => {
+export const checkUniquenessOfKeys = (keys, comp) => {
   if (new Set(keys).size !== keys.length) {
     throw errors.KEYS_ARE_NOT_UNIQUE(comp._compFnName, keys)
   }
 }
-
-export default checkUniquenessOfKeys
