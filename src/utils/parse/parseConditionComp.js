a6af74ad4efc7afa88594dd885040edb65d6e941
Manan Tank <manantankm@gmail.com>
2021-03-26 13:59:19 +0530

    fix parseConditionComp

---
+++ src/utils/parse/parseConditionComp.js
@@ -0,0 +1,23 @@
+import { ELSE_ATTRIBUTE, ENTER_ATTRIBUTE, EXIT_ATTRIBUTE } from '../../constants'
+import { removeAttr, getAnimationAttributes } from '../node/dom'
+import { processPlaceholder } from '../string/placeholder/processPlaceholder'
+
+/**
+ *
+ * @param {import('types/dom').ConditionalComp} comp
+ * @param {import('types/parsed').ConditionAttributes} conditionType
+ * @param {string} attributeValue
+ */
+export const parseConditionComp = (comp, conditionType, attributeValue) => {
+  comp._parsedInfo = {
+    ...comp._parsedInfo,
+    _conditionType: conditionType,
+    _animationAttributes: getAnimationAttributes(comp)
+  }
+  if (conditionType !== ELSE_ATTRIBUTE) {
+    /** @type {import('types/dom').IfComp}*/
+    (comp)._parsedInfo._conditionAttribute = processPlaceholder(attributeValue)
+  };
+
+  [ENTER_ATTRIBUTE, EXIT_ATTRIBUTE, conditionType].forEach(att => removeAttr(comp, att))
+}
