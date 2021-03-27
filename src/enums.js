4a3ff90fd90bbb3d0b378f2b980d2c9c2ceca5bf
Manan Tank <manantankm@gmail.com>
2021-03-27 15:11:07 +0530

    update enums to have strict value as type

--- src/enums.js
+++ src/enums.js
@@ -1,29 +1,33 @@
-// attribute types
 export const attributeTypes = {
-  _normal: 0,
-  _event: 1,
-  _state: 2,
-  _prop: 3,
-  _conditional: 4,
-  _staticState: 5,
-  _functional: 6,
-  _ref: 7
+  _normal: /** @type {0}*/ (0),
+  _event: /** @type {1}*/ (1),
+  _state: /** @type {2}*/ (2),
+  _prop: /** @type {3}*/ (3),
+  _conditional: /** @type {4}*/ (4),
+  _staticState: /** @type {5}*/ (5),
+  _functional: /** @type {6}*/ (6),
+  _ref: /** @type {7}*/ (7)
 }
 
 export const placeholderTypes = {
-  _reactive: 0,
-  _functional: 1
+  _reactive: /** @type {0}*/ (0),
+  _functional: /** @type {1}*/ (1)
 }
 
 export const reactiveModes = {
-  _detective: 0,
-  _reactive: 1,
-  _noOverride: 2,
-  _returnComp: 3
+  _detective: /** @type {0}*/ (0),
+  _reactive: /** @type {1}*/ (1),
+  _noOverride: /** @type {2}*/ (2),
+  _returnComp: /** @type {3}*/ (3)
 }
 
 export const conditionTypes = {
-  _if: 0,
-  _elseIf: 1,
-  _else: 2
+  _if: /** @type {0}*/ (0),
+  _elseIf: /** @type {1}*/ (1),
+  _else: /** @type {2}*/ (2)
+}
+
+export const batches = {
+  _beforeDOM: /** @type {0}*/ (0),
+  _DOM: /** @type {1}*/ (1)
 }
