0b8df8acdb87dd911963baef4fc8fe614532c33c
Manan Tank <manantankm@gmail.com>
2021-05-15 10:30:24 +0530

    fix invokeCompJs call position in onFirstConnect

--- src/utils/component/onFirstConnect.js
+++ src/utils/component/onFirstConnect.js
@@ -16,6 +16,9 @@ export function onFirstConnect (comp, compDef) {
   // create state
   comp.$ = reactify(comp, comp._prop$ || {}, [])
 
+  // after everything is set up, invoke js
+  if (compDef.js) invokeCompJs(compDef.js, comp)
+
   // manually created looped component requires hydration
   if (comp._isLooped) {
     hydrateAttributes(comp, comp._parsedInfo._attributes, comp)
@@ -29,8 +32,5 @@ export function onFirstConnect (comp, compDef) {
 
   // keep the attributes of comp element in sync
   subscribeNode(comp)
-
-  // after everything is set up, invoke js
-  if (compDef.js) invokeCompJs(compDef.js, comp)
 
 }
