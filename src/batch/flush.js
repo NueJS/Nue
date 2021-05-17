442da95887f06ecc5100880c4f2056848ce474da
Manan Tank <manantankm@gmail.com>
2021-05-17 12:01:25 +0530

    minor-refactor: flush.js

--- src/batch/flush.js
+++ src/batch/flush.js
@@ -7,13 +7,13 @@ import { flushBatch } from './flushBatch'
 export const flush = (comp) => {
 
   const { _beforeUpdate, _afterUpdate } = comp._eventCbs
-  const { _mutations } = comp
+  const { _mutations, _batches } = comp
 
   // before updates
   _beforeUpdate.forEach(cb => cb(_mutations))
 
   // updates
-  comp._batches.forEach(batch => flushBatch(batch, _mutations))
+  _batches.forEach(batch => flushBatch(batch, _mutations))
 
   // after updates
   _afterUpdate.forEach(cb => cb(_mutations))
