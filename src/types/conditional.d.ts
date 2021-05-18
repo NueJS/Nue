81a34c284f5b2257baeffc3cba20b3ddb067bc19
Manan Tank <manantankm@gmail.com>
2021-05-18 16:54:28 +0530

    refactor types

--- src/types/conditional.d.ts
+++ src/types/conditional.d.ts
@@ -1,23 +1,3 @@
-/** parsed object type of the component element that has either *else-if or *else attribute */
-interface ConditionalComp_ParseInfo extends Comp_ParseInfo {
-	_animationAttributes: AnimationAttributes_ParseInfo;
-	_conditionType: ConditionAttribute;
-}
-
-/** parsed object type of the component element with *if attribute */
-interface IfComp_ParseInfo extends ConditionalComp_ParseInfo {
-	_conditionAttribute: Placeholder;
-	_conditionGroup: (IfComp | ElseComp | ElseIfComp)[];
-	_conditionGroupStateDeps: StatePath[];
-	_conditionGroupAnchor: Comment;
-}
-
-/** parsed object type of the component element with *else attribute */
-interface ElseComp_ParseInfo extends ConditionalComp_ParseInfo {}
-
-/** parsed object type of the component element with *else-if attribute */
-interface ElseIfComp_ParseInfo extends IfComp_ParseInfo {}
-
 interface IfComp extends Comp {
 	_parsedInfo: IfComp_ParseInfo;
 }
--- src/types/parsedInfo.d.ts
+++ src/types/parsedInfo.d.ts
@@ -29,3 +29,23 @@ interface LoopedComp_ParseInfo extends Comp_ParseInfo {
 	_animationAttributes: AnimationAttributes_ParseInfo;
 	_loopAttributes: LoopAttributes;
 }
+
+/** parsed object type of the component element that has either *else-if or *else attribute */
+interface ConditionalComp_ParseInfo extends Comp_ParseInfo {
+	_animationAttributes: AnimationAttributes_ParseInfo;
+	_conditionType: ConditionAttribute;
+}
+
+/** parsed object type of the component element with *if attribute */
+interface IfComp_ParseInfo extends ConditionalComp_ParseInfo {
+	_conditionAttribute: Placeholder;
+	_conditionGroup: (IfComp | ElseComp | ElseIfComp)[];
+	_conditionGroupStateDeps: StatePath[];
+	_conditionGroupAnchor: Comment;
+}
+
+/** parsed object type of the component element with *else attribute */
+interface ElseComp_ParseInfo extends ConditionalComp_ParseInfo {}
+
+/** parsed object type of the component element with *else-if attribute */
+interface ElseIfComp_ParseInfo extends IfComp_ParseInfo {}
