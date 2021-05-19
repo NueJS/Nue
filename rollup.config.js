156b1886b46a04b652aa24eba227c0424de11dc2
Manan Tank <manantankm@gmail.com>
2021-05-19 14:29:51 +0530

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,75 +1,73 @@
-import replace from 'rollup-plugin-replace'
-import { terser } from 'rollup-plugin-terser'
+import replace from 'rollup-plugin-replace';
+import { terser } from 'rollup-plugin-terser';
 // import typescript from '@rollup/plugin-typescript'
 
 const minifier = [
-  terser({
-    compress: true,
-    mangle: {
-      properties: {
-        regex: /^_/ // mangle properties whose name starts with _
-      }
-    }
-  })
-]
+	terser({
+		compress: true,
+		mangle: {
+			properties: {
+				regex: /^_/, // mangle properties whose name starts with _
+			},
+		},
+	}),
+];
 
-// (Node)
+// CJS (Node.js)
 const cjsProd = {
-  file: './dist/nue.cjs.prod.js',
-  format: 'cjs',
-  plugins: minifier
-}
+	file: './dist/nue.cjs.prod.js',
+	format: 'cjs',
+	plugins: minifier,
+};
 
 const cjsDev = {
-  file: './dist/nue.cjs.dev.js',
-  format: 'cjs'
-}
+	file: './dist/nue.cjs.dev.js',
+	format: 'cjs',
+};
 
 // UMD (Browser + Node)
 const umdProd = {
-  file: './dist/nue.umd.prod.js',
-  format: 'umd',
-  plugins: minifier,
-  name: 'nue'
-}
+	file: './dist/nue.umd.prod.js',
+	format: 'umd',
+	plugins: minifier,
+	name: 'nue',
+};
 
 const umdDev = {
-  file: './dist/nue.umd.dev.js',
-  format: 'umd',
-  name: 'nue'
-}
+	file: './dist/nue.umd.dev.js',
+	format: 'umd',
+	name: 'nue',
+};
 
+// ESM
 const es = {
-  file: './dist/nue.es.js',
-  format: 'es',
-  plugins: minifier
-}
+	file: './dist/nue.es.js',
+	format: 'es',
+	plugins: minifier,
+};
 
-const outputs = {
-  cjsProd,
-  cjsDev,
-  umdProd,
-  umdDev,
-  es
-}
+const outputConfigs = {
+	cjsProd,
+	cjsDev,
+	umdProd,
+	umdDev,
+	es,
+};
 
 export default info => {
-  let replaceValues = {
-    _DEV_: Boolean(info.dev)
-  }
+	// on ESM, replace _DEV_ with process.env.NODE_ENV !== 'production
+	// for others, replace _DEV_ with either true or false
+	let replaceValues = {
+		_DEV_: info.bundle === 'es' ? "process.env.NODE_ENV !== 'production'" : Boolean(info.dev),
+	};
 
-  if (info.bundle === 'es') {
-    replaceValues = {
-      _DEV_: "process.env.NODE_ENV !== 'production'"
-    }
-  }
-  return {
-    input: './src/index.js',
-    output: outputs[info.bundle],
-    plugins: [
-      replace({
-        values: replaceValues
-      })
-    ]
-  }
-}
+	return {
+		input: './src/index.js',
+		output: outputConfigs[info.bundle],
+		plugins: [
+			replace({
+				values: replaceValues,
+			}),
+		],
+	};
+};
